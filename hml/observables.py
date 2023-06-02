239b9463a514b96c04926e2e5e47997ca5f1c451
Star9daisy <star9daisy@outlook.com>
2023-06-02 20:27:13 +0800

    feat: Add new observable Mass

--- hml/observables.py
+++ hml/observables.py
@@ -66,3 +66,37 @@ class PT:
     @property
     def values(self):
         return np.array(self._values, dtype=np.float32)
+
+class Mass:
+    def __init__(self, name=""):
+        self._name = name
+        self._values = None
+
+    def from_event(self, event: TTree):
+        names, indices = resolve_string(self._name)
+
+        if indices[0] == "all":
+            self._values = [i.Mass for i in getattr(event, names[0])]
+        else:
+            combined_object = TLorentzVector()
+            for name, index in zip(names, indices):
+                branch = getattr(event, name)
+                if branch.GetEntries() <= index + 1:
+                    raise IndexError(
+                        f"Index {index} out of range for branch {name} with {branch.GetEntries()} entries"
+                    )
+                combined_object += branch[index].P4()
+
+            self._values = [combined_object.M()]
+
+    def from_branch(self, branch: TClonesArray):
+        self._values = [i.P4().M() for i in branch]
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def values(self):
+        return np.array(self._values, dtype=np.float32)
+
