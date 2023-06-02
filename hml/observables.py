9d8add96136da1042c1cdd53b124c833cc77730c
Star9daisy <star9daisy@outlook.com>
2023-06-02 20:31:17 +0800

    feat: Add new observable Phi

--- hml/observables.py
+++ hml/observables.py
@@ -67,6 +67,7 @@ class PT:
     def values(self):
         return np.array(self._values, dtype=np.float32)
 
+
 class Mass:
     def __init__(self, name=""):
         self._name = name
@@ -125,6 +126,40 @@ class Eta:
 
     def from_branch(self, branch: TClonesArray):
         self._values = [i.P4().Eta() for i in branch]
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def values(self):
+        return np.array(self._values, dtype=np.float32)
+
+
+class Phi:
+    def __init__(self, name=""):
+        self._name = name
+        self._values = None
+
+    def from_event(self, event: TTree):
+        names, indices = resolve_string(self._name)
+
+        if indices[0] == "all":
+            self._values = [i.Phi for i in getattr(event, names[0])]
+        else:
+            combined_object = TLorentzVector()
+            for name, index in zip(names, indices):
+                branch = getattr(event, name)
+                if branch.GetEntries() <= index + 1:
+                    raise IndexError(
+                        f"Index {index} out of range for branch {name} with {branch.GetEntries()} entries"
+                    )
+                combined_object += branch[index].P4()
+
+            self._values = [combined_object.Phi()]
+
+    def from_branch(self, branch: TClonesArray):
+        self._values = [i.P4().Phi() for i in branch]
 
     @property
     def name(self):
