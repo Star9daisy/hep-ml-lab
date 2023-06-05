a6c23d3e9aac1b56eff0f3845dbedf369067366e
Star9daisy <star9daisy@outlook.com>
2023-06-05 21:33:52 +0800

    perf: Update representation Set

--- hml/representations.py
+++ hml/representations.py
@@ -1,19 +1,20 @@
 from __future__ import annotations
-from .observables import Observable
+
 import numpy as np
+
+from .observables import Observable
 
 
 class Set:
     def __init__(self, observables: list[Observable]):
         self.observables = observables
-        self.obs_names = [obs.obs_name for obs in observables]
-        self._values = []
 
     @property
     def values(self):
         return np.array(self._values)
 
     def from_event(self, event):
+        self._values = []
         for obs in self.observables:
             obs.from_event(event)
-            self._values.append(obs.value)
+            self._values.append(obs.values)
