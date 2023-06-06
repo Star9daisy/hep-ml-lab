cd210a3e24938e4b8a7a920300083340b56ec1fe
Star9daisy <star9daisy@outlook.com>
2023-06-06 22:17:56 +0800

    feat: Update Dataset class

--- hml/datasets.py
+++ hml/datasets.py
@@ -1,20 +1,48 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from pathlib import Path
+
 import numpy as np
+import yaml
+from numpy import ndarray
 
 
+@dataclass
 class Dataset:
-    def __init__(self) -> None:
-        self._data = []
-        self._target = []
-        self.target_names = []
+    data: ndarray
+    target: ndarray
+    feature_names: list[str]
+    target_names: list[str]
+    description: str
+    dirpath: str
 
-    def add(self, data, target):
-        self._data.append(data)
-        self._target.append(target)
+    def save(self, exist_ok: bool = False):
+        dirpath = Path(self.dirpath)
+        dirpath.mkdir(parents=True, exist_ok=exist_ok)
 
-    @property
-    def data(self):
-        return np.array(self._data)
+        metadata = {
+            "feature_names": self.feature_names,
+            "target_names": self.target_names,
+            "description": self.description,
+            "dirpath": self.dirpath,
+        }
+        with open(dirpath / "metadata.yml", "w") as f:
+            yaml.dump(metadata, f)
+        np.savez(dirpath / "metadata.yml", data=self.data, target=self.target)
 
-    @property
-    def target(self):
-        return np.array(self._target)
+    @classmethod
+    def from_path(cls, dirpath: str) -> Dataset:
+        dirpath = Path(dirpath)
+        with open(dirpath / "metadata.yml", "r") as f:
+            metadata = yaml.safe_load(f)
+        dataset = np.load(dirpath / "dataset.npz")
+
+        return cls(
+            data=dataset["data"],
+            target=dataset["target"],
+            feature_names=metadata["feature_names"],
+            target_names=metadata["target_names"],
+            description=metadata["description"],
+            dirpath=metadata["dirpath"],
+        )
