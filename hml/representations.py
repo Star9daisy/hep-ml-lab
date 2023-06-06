ebe9588ad7c8117aa72b1e23de411e667c295869
Star9daisy <star9daisy@outlook.com>
2023-06-06 21:37:04 +0800

    perf: Update representation "Set"

--- hml/representations.py
+++ hml/representations.py
@@ -8,13 +8,13 @@ from .observables import Observable
 class Set:
     def __init__(self, observables: list[Observable]):
         self.observables = observables
-
-    @property
-    def values(self):
-        return np.array(self._values)
+        self.names = [obs.name for obs in observables]
+        self.values = None
 
     def from_event(self, event):
-        self._values = []
         for obs in self.observables:
             obs.from_event(event)
-            self._values.append(obs.values)
+
+        # Stack 1D values of observables into 2D array
+        self.values = np.stack([obs.values for obs in self.observables], axis=0)
+        self.values = np.squeeze(self.values, axis=1)
