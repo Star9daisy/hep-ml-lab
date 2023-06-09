f53f410b03652b949050dff43a8e2a64de0c2b35
Star9daisy <star9daisy@outlook.com>
2023-06-09 20:57:20 +0800

    feat: Add a base model "KerasModel"

--- hml/models/base.py
+++ hml/models/base.py
@@ -1,4 +1,8 @@
+import sys
+from io import StringIO
 from typing import Any, Protocol
+
+from keras.utils.layer_utils import count_params
 
 
 class Model(Protocol):
@@ -13,3 +17,38 @@ class Model(Protocol):
 
     def save(self, path: str, suffix: str):
         pass
+
+
+class KerasModel:
+    def __init__(self, model, name="keras_model"):
+        self.model = model
+        self.name = model.name if not name else name
+
+    def compile(self, *args, **kwargs):
+        self.model.compile(*args, **kwargs)
+
+    def fit(self, *args, **kwargs):
+        self.model.fit(*args, **kwargs)
+
+    def predict(self, x, *args, **kwargs):
+        y_prob = self.model.predict(x, *args, **kwargs)
+        y_pred = y_prob.argmax(1)
+        return y_pred
+
+    def predict_proba(self, x, *args, **kwargs):
+        y_prob = self.model.predict(x, *args, **kwargs)
+        return y_prob
+
+    @property
+    def description(self):
+        stdout = sys.stdout
+        output_buffer = StringIO()
+        sys.stdout = output_buffer
+        self.model.summary()
+        sys.stdout = stdout
+        return output_buffer.getvalue()
+
+    @property
+    def n_parameters(self):
+        trainable_params = count_params(self.model.trainable_weights)
+        return trainable_params
