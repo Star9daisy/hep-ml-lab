4a9b068d7a740982703b45561b68ef0f1ed93fd9
Star9daisy <star9daisy@outlook.com>
2023-06-10 21:05:26 +0800

    feat: Add new model "B-L"

---
+++ hml/models/B-L/B-L-4_UFO/coupling_orders.py
@@ -0,0 +1,16 @@
+# This file was automatically created by FeynRules 2.3.24
+# Mathematica version: 12.0.0 for Mac OS X x86 (64-bit) (April 7, 2019)
+# Date: Wed 9 Jun 2021 22:04:10
+
+
+from object_library import all_orders, CouplingOrder
+
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
