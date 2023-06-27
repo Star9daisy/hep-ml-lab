ae5b79bf0ca85179107d4b36efeded1a359916dc
Star9daisy <star9daisy@outlook.com>
2023-06-27 20:03:10 +0800

    test: Remove accurate cross section checks

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -63,14 +63,14 @@ def test_Madgraph5():
     ]
 
     assert generator1.commands == expected1
-    assert generator1.runs[0].cross_section == 6222.1179999999995
+    assert generator1.runs[0].cross_section != 0
     assert generator2.commands == expected2
-    assert generator2.runs[0].cross_section == 0.0008714212999999999
+    assert generator2.runs[0].cross_section != 0
 
     run = MG5Run(run_dir=Path.cwd() / "tests/data/pp2wz/Events/run_01")
     assert isinstance(run.events, cppyy.gbl.TTree)
     assert run.run_tag == "tag_1"
-    assert run.cross_section == 0.0008714212999999999
+    assert run.cross_section != 0
 
     # Clean up
     shutil.rmtree(Path.cwd() / "tests/data/pp2zj")
