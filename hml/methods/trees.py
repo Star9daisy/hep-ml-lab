69c950eb9842e00cc5322ebf53f32ddb1ea16469
Star9daisy <star9daisy@outlook.com>
2023-07-09 16:04:26 +0800

    chore: Remove "Any" type

--- hml/methods/trees.py
+++ hml/methods/trees.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import pickle
 from importlib import import_module
 from pathlib import Path
-from typing import Any
 
 from keras.metrics import Metric
 from keras.utils import to_categorical
