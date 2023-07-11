51ab089df59ac29778a6c31973c661ce6b06b5e6
Star9daisy <star9daisy@outlook.com>
2023-07-11 12:10:39 +0800

    doc: Add comments to "MaxSignificance" and "RejectionAtEfficiency"

--- hml/metrics.py
+++ hml/metrics.py
@@ -8,7 +8,34 @@ from keras.metrics import Metric, SpecificityAtSensitivity
 
 @tf.keras.utils.register_keras_serializable()
 class MaxSignificance(Metric):
-    """Calculate the maximum significance of a model's predictions."""
+    """Calculate the maximum significance of a model's predictions.
+
+    The significance is obtained by correctly identified signal events and incorrectly identified
+    background events (tagged as signal). The formula taken here is:
+
+    $$
+    significance = \frac{S}{\sqrt{B}}
+    $$
+
+    where $S$ is the number of correctly identified signal events (true positive) and $B$ is the
+    number of incorrectly identified background events (false positive). Methods usually give
+    probabilities for each class, so a threshold is needed to decide whether an event is signal or
+    background. As a result, one threshold corresponds to one significance value. This is why this
+    metric is called `MaxSignificance`: it calculates the maximum significance value among all the
+    thresholds.
+
+    Parameters
+    ----------
+    n_thresholds : int, optional
+        Number of thresholds to use to compute the significance. The thresholds are uniformly
+        distributed in the [0, 1] range. The default is 101.
+    class_id : int, optional
+        Class ID of the signal class. The default is 1.
+    name : str, optional
+        Name of the metric. The default is "max_significance".
+    dtype : tf.dtypes.DType, optional
+        Data type of the metric. The default is tf.float32.
+    """
 
     def __init__(
         self,
@@ -20,6 +47,8 @@ class MaxSignificance(Metric):
         super().__init__(name=name, dtype=dtype)
 
         # Compute thresholds in [0, 1] range
+        # If the number of thresholds is 1, then the threshold is 0.5 and this metric is equivalent
+        # to the significance metric.
         if n_thresholds == 1:
             self.thresholds = [0.5]
         else:
@@ -43,6 +72,17 @@ class MaxSignificance(Metric):
         y_pred: list | np.ndarray | tf.Tensor,
         sample_weight: list | np.ndarray | tf.Tensor | None = None,
     ):
+        """Update the state of the metric in a batch-wise fashion.
+
+        Parameters
+        ----------
+        y_true : list | np.ndarray | tf.Tensor
+            True labels.
+        y_pred : list | np.ndarray | tf.Tensor
+            Predicted values (probabilities).
+        sample_weight : list | np.ndarray | tf.Tensor | None, optional
+            Sample weights. The default is None.
+        """
         y_true = tf.convert_to_tensor(y_true)
         y_pred = tf.convert_to_tensor(y_pred)
         sample_weight = tf.convert_to_tensor(sample_weight) if sample_weight is not None else None
@@ -70,6 +110,13 @@ class MaxSignificance(Metric):
                 )
 
     def result(self) -> tf.Tensor:
+        """Return the maximum significance value.
+
+        Returns
+        -------
+        tf.Tensor
+            Maximum significance value.
+        """
         significances = [
             tp / tf.sqrt(fp + tf.keras.backend.epsilon())
             for tp, fp in zip(self.true_positives, self.false_positives)
@@ -82,7 +129,7 @@ class MaxSignificance(Metric):
         return self.max_significance
 
     def reset_state(self) -> None:
-        # The state of the metric will be reset at the start of each epoch.
+        """Reset the state of the metric for a new batch."""
         for i in range(len(self.thresholds)):
             self.true_positives[i].assign(0.0)
             self.false_positives[i].assign(0.0)
@@ -90,6 +137,28 @@ class MaxSignificance(Metric):
 
 @tf.keras.utils.register_keras_serializable()
 class RejectionAtEfficiency(Metric):
+    """Calculate the background rejection at a given signial efficiency.
+
+    This metric is for binary classification problems. The background rejection is the inverse of
+    the mistag rate, which is the fraction of background events that are incorrectly identified as
+    signal. The signal efficiency is the fraction of signal events that are correctly identified as
+    signal. The signal efficiency and the mistag rate are true positive and false positive rates.
+
+    Parameters
+    ----------
+    efficiency : float, optional
+        Signal efficiency. The default is 0.5.
+    n_thresholds : int, optional
+        Number of thresholds to use to compute the rejection. The thresholds are uniformly
+        distributed in the [0, 1] range. The default is 101.
+    class_id : int, optional
+        Class ID of the signal class. The default is 1.
+    name : str, optional
+        Name of the metric. The default is "rejection_at_efficiency".
+    dtype : tf.dtypes.DType, optional
+        Data type of the metric. The default is tf.float32.
+    """
+
     def __init__(
         self,
         efficiency: float = 0.5,
@@ -113,12 +182,31 @@ class RejectionAtEfficiency(Metric):
         y_pred: list | np.ndarray | tf.Tensor,
         sample_weight: list | np.ndarray | tf.Tensor | None = None,
     ):
+        """Update the state of the metric in a batch-wise fashion.
+
+        Parameters
+        ----------
+        y_true : list | np.ndarray | tf.Tensor
+            True labels.
+        y_pred : list | np.ndarray | tf.Tensor
+            Predicted values (probabilities).
+        sample_weight : list | np.ndarray | tf.Tensor | None, optional
+            Sample weights. The default is None.
+        """
         self.specificity_at_sensitivity.update_state(y_true, y_pred, sample_weight)
 
     def result(self) -> tf.Tensor:
+        """Return the background rejection at the given signal efficiency.
+
+        Returns
+        -------
+        tf.Tensor
+            Background rejection.
+        """
         specificity = 1 - self.specificity_at_sensitivity.result()
         rejection = 1 / (specificity + tf.keras.backend.epsilon())
         return rejection
 
     def reset_state(self) -> None:
+        """Reset the state of the metric for a new batch."""
         self.specificity_at_sensitivity.reset_states()
