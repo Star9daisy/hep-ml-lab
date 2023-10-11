b39bfb03b57c140b16a73659e8601ae40cf9128b
Star9daisy <star9daisy@outlook.com>
2023-10-11 07:42:59 +0000

    feat: Add a new representation "Set"

---
+++ hml/new_representations.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+import numpy as np
+import pandas as pd
+
+from .new_observables import Observable, get_observable
+
+
+class Set:
+    def __init__(self, observables: list[Observable | str]) -> None:
+        self.observables = []
+        for obs in observables:
+            if isinstance(obs, Observable):
+                self.observables.append(obs)
+            elif isinstance(obs, str):
+                self.observables.append(get_observable(obs))
+            else:
+                raise TypeError(f"Invalid type {type(obs)} for observable {obs}")
+
+        self.names = [obs.name for obs in self.observables]
+        self.values = []
+
+    def read(self, event) -> None:
+        for obs in self.observables:
+            obs.read(event)
+
+        self.values = [obs.value for obs in self.observables]
+
+    def to_numpy(self) -> np.ndarray:
+        return np.array(self.values)
+
+    def to_pandas(self) -> pd.DataFrame:
+        return pd.DataFrame([self.values], columns=self.names)
