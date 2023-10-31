120a72d3298c195218072e1f44cd1ed53bcb7d3f
Star9daisy <star9daisy@outlook.com>
2023-10-31 13:49:59 +0000

    feat: Expose the classes to the outermost layer

---
+++ hml/approaches/__init__.py
@@ -0,0 +1,8 @@
+# Cuts
+from .cuts import CutAndCount  # isort: skip
+
+# Trees
+from .trees import GradientBoostedDecisionTree  # isort: skip
+
+# Networks
+from .networks.mlps import ToyMultilayerPerceptron  # isort: skip
