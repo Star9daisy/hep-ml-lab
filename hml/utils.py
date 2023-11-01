484847756440ba4ad6dfc9502dbcf13107b519cb
Star9daisy <star9daisy@outlook.com>
2023-11-01 08:28:44 +0000

    doc: Add comments to Filter

--- hml/utils.py
+++ hml/utils.py
@@ -7,16 +7,28 @@ from .observables import get_observable
 
 
 class Filter:
+    """Pre-select events based on a list of cuts.
+
+    Parameters
+    ----------
+    cuts : list[str]
+        The list of cuts to apply.
+    """
+
     def __init__(self, cuts: list[str]) -> None:
         self.cuts = cuts
         self.stat = OrderedDict({cut: 0 for cut in cuts})
 
     def read_event(self, event):
+        """Read the event from a TTree."""
         self.event = event
         return self
 
     def passed(self):
+        """Check if the event passed the cuts."""
+
         def _replace_with_value(match):
+            """Replace the string name of an observable with its value."""
             observable_name = match.group(0)  # complete match
             value = get_observable(observable_name).read_event(self.event).value
 
