c00e4d90f5d68e01045ba0acab6b211b81f4717f
Star9daisy <star9daisy@outlook.com>
2023-11-04 08:24:51 +0000

    chore: Remove lagacy test

--- tests/test_preprocessing.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def test_is_categorical():
-    from hml.preprocessing import is_categorical
-
-    assert is_categorical(np.array([[0, 1], [1, 0], [1, 0], [0, 1]])) == True
-    assert is_categorical(np.array([[0, 2], [1, 0], [1, 0], [0, 1]])) == False
-    assert is_categorical(np.array([[0, 0], [1, 0], [1, 0], [0, 1]])) == False
