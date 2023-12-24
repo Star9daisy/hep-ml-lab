fb6e29fb3d6295ac194c59a1ef0b5e9fd8b1bf8a
Star9daisy <star9daisy@outlook.com>
2023-12-24 08:29:56 +0000

    feat: Add CutLayer to handle one distribution per time

--- hml/approaches/cuts.py
+++ hml/approaches/cuts.py
@@ -1,5 +1,7 @@
 import keras
 import tensorflow as tf
+from typing import Callable
+from keras import losses, ops
 
 
 @keras.saving.register_keras_serializable()
@@ -235,3 +237,206 @@ class CutAndCount(keras.Model):
                 print(f"  #{i}: {cuts[0]:.4f} <= x <= {cuts[1]:.4f}")
             else:
                 print(f"  #{i}: x <= {cuts[0]:.4f} or x >= {cuts[1]:.4f}")
+
+
+def ops_histogram_fixed_width(values, value_range, nbins, dtype="int32"):
+    value_min, value_max = value_range
+    bin_edges = ops.linspace(value_min, value_max, nbins + 1)
+    lower = bin_edges[:-1]
+    upper = bin_edges[1:]
+
+    return ops.fori_loop(
+        0,
+        nbins,
+        lambda i, s: ops.scatter_update(
+            s,
+            [[i]],
+            [
+                ops.count_nonzero(
+                    ops.where(
+                        ops.logical_and(lower[i] <= values, values <= upper[i]), 1, 0
+                    )
+                )
+            ],
+        ),
+        ops.zeros((nbins,), dtype=dtype),
+    )
+
+
+def ops_unique(tensor):
+    sorted_tensor, sorted_indices = ops.sort(tensor), ops.argsort(tensor)
+    unique_indices = ops.append(
+        [0], ops.where(ops.not_equal(sorted_tensor[1:], sorted_tensor[:-1]))[0] + 1
+    )
+    unique_elements = ops.take(sorted_tensor, unique_indices)
+    # unique_indices = ops.take(sorted_indices, unique_indices)
+
+    return unique_elements
+
+
+class CutLayer(keras.layers.Layer):
+    def __init__(
+        self,
+        cut=None,
+        case=None,
+        n_bins=50,
+        feature_id=0,
+        loss_fn: Callable | str = "binary_crossentropy",
+    ):
+        super().__init__()
+
+        self.n_bins = n_bins
+        self.feature_id = feature_id
+
+        self.loss_fn = losses.get(loss_fn)
+
+        self.cut = self.add_weight(
+            shape=(2,),
+            initializer="zeros",
+            trainable=False,
+        )
+        if cut is not None:
+            self.cut.assign(cut)
+
+        self.case = self.add_weight(
+            shape=(),
+            initializer="zeros",
+            trainable=False,
+        )
+        if case is not None:
+            self.case.assign(case)
+
+    def call(self, inputs, targets=None, sample_weight=None):
+        self.sample_weight = sample_weight
+        x = ops.convert_to_tensor(inputs, "float32")
+
+        if targets is not None and self.loss_fn is not None:
+            y = ops.convert_to_tensor(targets, "int32")
+            y = ops.argmax(y, -1) if y.ndim > 1 else y
+
+            x = ops.take(inputs, self.feature_id, -1)
+            x_min = ops.min(x)
+            x_max = ops.max(x)
+            bin_edges = ops.linspace(x_min, x_max, self.n_bins + 1)
+
+            is_bkg = ops.where(ops.equal(y, 0))
+            is_sig = ops.where(ops.equal(y, 1))
+
+            is_sig = is_sig[0] if isinstance(is_sig, tuple) else is_sig
+            is_bkg = is_bkg[0] if isinstance(is_bkg, tuple) else is_bkg
+
+            is_sig = ops.convert_to_tensor(is_sig, "int32")
+            is_bkg = ops.convert_to_tensor(is_bkg, "int32")
+            is_sig = ops.squeeze(is_sig, 0) if is_sig.ndim > 1 else is_sig
+            is_bkg = ops.squeeze(is_bkg, 0) if is_bkg.ndim > 1 else is_bkg
+
+            x0 = ops.take(x, is_bkg)
+            x1 = ops.take(x, is_sig)
+
+            hist0 = ops_histogram_fixed_width(x0, [x_min, x_max], self.n_bins)
+            hist1 = ops_histogram_fixed_width(x1, [x_min, x_max], self.n_bins)
+
+            curr_case0 = ops.greater(hist0, hist1)[:-1]
+            next_case0 = ops.greater(hist0, hist1)[1:]
+            is_on_boundary0 = ops.logical_xor(curr_case0, next_case0)
+            is_not_empty0 = hist0[:-1] > 0
+            is_candidate0 = ops.logical_and(is_on_boundary0, is_not_empty0)
+            candidate_indices0 = ops.squeeze(ops.where(is_candidate0), 0) + 1
+            candidates0 = ops.take(bin_edges, candidate_indices0)
+
+            curr_case1 = ops.greater(hist1, hist0)[:-1]
+            next_case1 = ops.greater(hist1, hist0)[1:]
+            is_on_boundary1 = ops.logical_xor(curr_case1, next_case1)
+            is_not_empty1 = hist1[:-1] > 0
+            is_candidate1 = ops.logical_and(is_on_boundary1, is_not_empty1)
+            candidate_indices1 = ops.squeeze(ops.where(is_candidate1), 0) + 1
+            candidates1 = ops.take(bin_edges, candidate_indices1)
+
+            candidates = ops_unique(ops.concatenate([candidates0, candidates1], 0))
+            candidates = ops.cond(
+                ops.equal(ops.shape(candidates), (1,)),
+                lambda: ops.append(candidates, x_max),
+                lambda: candidates,
+            )
+
+            i, j = ops.meshgrid(candidates, candidates)
+            i, j = i[i < j], j[i < j]
+            candidate_pairs = ops.stack([i, j], 1)
+            losses_and_cases = ops.vectorized_map(
+                lambda pair: self._get_min_loss_and_case(x, y, pair, self.loss_fn),
+                candidate_pairs,
+            )
+            min_index = ops.argmin(losses_and_cases[:, 0])
+            min_loss = losses_and_cases[min_index, 0]
+            min_case = losses_and_cases[min_index, 1]
+
+            lower = candidate_pairs[min_index, 0]
+            upper = candidate_pairs[min_index, 1]
+
+            self.cut.assign(ops.array([lower, upper]))
+            self.case.assign(min_case)
+
+        y_pred = ops.cond(
+            ops.equal(self.case, 0),
+            lambda: ops.where(x <= self.cut[0], 1, 0),
+            lambda: ops.cond(
+                ops.equal(self.case, 1),
+                lambda: ops.where(x >= self.cut[0], 1, 0),
+                lambda: ops.cond(
+                    ops.equal(self.case, 2),
+                    lambda: ops.where(
+                        ops.logical_and(x >= self.cut[0], x <= self.cut[1]), 1, 0
+                    ),
+                    lambda: ops.where(
+                        ops.logical_or(x <= self.cut[0], x >= self.cut[1]), 1, 0
+                    ),
+                ),
+            ),
+        )
+        return y_pred
+
+        # if self.cut == "":
+        #     raise ValueError("cut is not set")
+        # if "feature" not in self.cut:
+        #     raise ValueError("cut must contain 'feature'")
+
+        # feature = ops.take(inputs, self.feature_id, -1)
+        # y_pred = ops.cast(eval(self.cut), "int32")
+
+        # return y_pred
+
+    def _get_min_loss_and_case(self, x, y, candidate_pair, loss_fn):  # pragma: no cover
+        # I: (n_samples,), (n_samples, ?), (2,)
+        cut0 = candidate_pair[0]
+        cut1 = candidate_pair[1]
+
+        # For each pair, calculate the loss of the four cases
+        # Case 0: signal on the left
+        on_left = ops.less_equal(x, cut0)  # (n_samples,)
+        y_pred0 = ops.where(on_left, 1.0, 0.0)  # (n_samples,)
+        loss0 = loss_fn(y, y_pred0, self.sample_weight)
+
+        # Case 1: signal on the right
+        on_right = ops.greater_equal(x, cut0)
+        y_pred1 = ops.where(on_right, 1.0, 0.0)
+        loss1 = loss_fn(y, y_pred1, self.sample_weight)
+
+        # Case 2: signal in the middle
+        in_middle = ops.logical_and(x >= cut0, x <= cut1)
+        y_pred2 = ops.where(in_middle, 1.0, 0.0)
+        loss2 = loss_fn(y, y_pred2, self.sample_weight)
+
+        # Case 3: signal on both sides
+        on_both_sides = ops.logical_or(x <= cut0, x >= cut1)
+        y_pred3 = ops.where(on_both_sides, 1.0, 0.0)
+        loss3 = loss_fn(y, y_pred3, self.sample_weight)
+
+        # The cases 0, 1, 2, 3 are corresponding to the four possible cases:
+        # left, right, middle, both sides
+        min_case = ops.argmin([loss0, loss1, loss2, loss3])
+        min_loss = ops.min([loss0, loss1, loss2, loss3])
+
+        return ops.append(min_loss, min_case)  # (2,)
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
