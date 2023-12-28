6924e54318e07cd5906fdb3bbb7cbedf702ce06e
Star9daisy <star9daisy@outlook.com>
2023-12-28 09:47:36 +0000

    config: Improve readibility of top imports

--- hml/__init__.py
+++ hml/__init__.py
@@ -1,11 +1,11 @@
-from hml import (
-    __version__,
-    approaches,
-    datasets,
-    generators,
-    metrics,
-    observables,
-    representations,
-    types,
-    utils,
-)
+# isort:skip_file
+
+from hml import generators
+from hml import observables
+from hml import representations
+from hml import datasets
+from hml import approaches
+from hml import metrics
+from hml import types
+from hml import utils
+from hml.version import __version__
