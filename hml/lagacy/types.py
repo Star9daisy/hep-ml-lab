cccc6397e042dbd2abb4aa0ca4200224821a344e
Star9daisy <star9daisy@outlook.com>
2024-01-01 03:09:14 +0000

    chore: Move lagacy to archive folder

--- hml/lagacy/types.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-from typing import Any, Literal, Union
-
-PathLike = Union[str, Path]
-ShowerOption = Literal["", "off", "pythia8"]
-DetectorOption = Literal["", "off", "delphes"]
