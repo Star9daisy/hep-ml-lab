810fa84190a8a6d9b7156f7a744ecbe7c565fa0c
Star9daisy <star9daisy@outlook.com>
2024-01-02 03:33:14 +0000

    chore: Improve readibility

--- hml/observables/charge.py
+++ hml/observables/charge.py
@@ -3,14 +3,15 @@ from ..types import Observable
 
 class Charge(Observable):
     def get_value(self):
-        if len(self.main_phyobjs) != 1:
+        if len(self.main_objs) != 1:
             return
 
-        if len(self.sub_phyobjs[0]) != 0:
+        if len(self.sub_objs[0]) != 0:
             return
 
-        values = [
-            obj.Charge if obj is not None else float("nan")
-            for obj in self.main_phyobjs[0]
-        ]
+        values = []
+        for obj in self.main_objs[0]:
+            value = obj.Charge if obj is not None else float("nan")
+            values.append(value)
+
         return values
