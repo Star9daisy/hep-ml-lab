ea9e7b82a75a6c1bc4bc1ddb763586c6cd4c7e40
Star9daisy <star9daisy@outlook.com>
2024-01-02 08:41:01 +0000

    feat: Add observable-agnostic function to get one according to name

--- hml/utils.py
+++ hml/utils.py
@@ -1,5 +1,18 @@
-def get_observable():
-    ...
+from .types import Observable
+
+
+def get_observable(name: str, **kwargs):
+    if len(parts := name.split(".")) == 1:
+        physics_object, classname = "", parts[0]
+    else:
+        physics_object, classname = parts
+
+    if classname not in Observable.ALL_OBSERVABLES:
+        raise ValueError(f"Observable {classname} not found")
+
+    return Observable.ALL_OBSERVABLES[classname](
+        physics_object=physics_object, **kwargs
+    )
 
 
 def save_dataset():
