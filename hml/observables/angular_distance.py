538a91a77c03b52ded643217849e5f87950296db
Star9daisy <star9daisy@outlook.com>
2024-01-02 12:47:25 +0000

    feat: Add new observable "DeltaR" and "InvMass"

--- hml/observables/angular_distance.py
+++ hml/observables/angular_distance.py
@@ -1,6 +1,71 @@
-class AngularDistance:
+from ..types import Observable
+from ..utils import get_observable
+
+
+class AngularDistance(Observable):
+    def __init__(self, physics_object, sorted_by="pt", reverse=True):
+        super().__init__(physics_object)
+        self.sorted_by = sorted_by
+        self.reverse = reverse
+        self.orders = [
+            get_observable(f"{i}.{sorted_by}") for i in physics_object.split(",")
+        ]
+
+    def get_value(self):
+        if len(self.main_objs) != 2:
+            return
+
+        for i in self.orders:
+            i.read(self.event)
+
+        if len(self.sub_objs[0]) != 0:
+            flat_obj0 = [i for subs in self.sub_objs[0] for i in subs]
+            flat_order0 = [i for subs in self.orders[0].value for i in subs]
+        else:
+            flat_obj0 = [i for i in self.main_objs[0]]
+            flat_order0 = [i for i in self.orders[0].value]
+
+        if len(self.sub_objs[1]) != 0:
+            flat_obj1 = [i for subs in self.sub_objs[1] for i in subs]
+            flat_order1 = [i for subs in self.orders[1].value for i in subs]
+        else:
+            flat_obj1 = [i for i in self.main_objs[1]]
+            flat_order1 = [i for i in self.orders[1].value]
+
+        sorted_obj0 = [
+            obj
+            for _, obj in sorted(
+                zip(flat_order0, flat_obj0),
+                key=lambda pair: pair[0],
+                reverse=self.reverse,
+            )
+        ]
+        sorted_obj1 = [
+            obj
+            for _, obj in sorted(
+                zip(flat_order1, flat_obj1),
+                key=lambda pair: pair[0],
+                reverse=self.reverse,
+            )
+        ]
+
+        values = []
+        for i in sorted_obj0:
+            distance_per_i = []
+
+            for j in sorted_obj1:
+                if i is None or j is None:
+                    distance_per_i.append(float("nan"))
+                else:
+                    distance_per_i.append(i.P4().DeltaR(j.P4()))
+            values.append(distance_per_i)
+
+        return values
+
+
+class DeltaR(AngularDistance):
     ...
 
 
-class DeltaR:
-    ...
+AngularDistance.add_alias("angular_distance")
+DeltaR.add_alias("delta_r")
--- hml/observables/invariant_mass.py
+++ hml/observables/invariant_mass.py
@@ -1,10 +1,32 @@
-class InvariantMass:
+from functools import reduce
+
+from ..types import Observable
+
+
+class InvariantMass(Observable):
+    def get_value(self):
+        for subs in self.sub_objs:
+            if len(subs) != 0:
+                return
+
+        values = []
+        sub_objs = []
+        for objs in self.main_objs:
+            sub_objs.append(reduce(lambda i, j: i.P4() + j.P4(), objs))
+
+        values.append(reduce(lambda i, j: i.P4() + j.P4(), sub_objs).M())
+
+        return values
+
+
+class InvMass(InvariantMass):
     ...
 
 
-class InvMass:
+class InvM(InvariantMass):
     ...
 
 
-class InvM:
-    ...
+InvariantMass.add_alias("invariant_mass")
+InvMass.add_alias("inv_mass")
+InvM.add_alias("inv_m")
