c0dab3e6e52c01a26976cea57789a3102927058b
Star9daisy <star9daisy@outlook.com>
2024-01-03 13:46:17 +0000

    fix: Cut incorrectly changed everytime it's called

--- hml/approaches/cuts/cut_layer.py
+++ hml/approaches/cuts/cut_layer.py
@@ -90,13 +90,13 @@ class CutLayer(keras.Layer):
             self._count += 1
             return False
 
-    def call(self, inputs, targets=None, sample_weight=None):
+    def call(self, inputs, targets=None, sample_weight=None, training=None):
         x = inputs
         y = targets
         x = ops.take(x, self.feature_id, -1)
         self.sample_weight = sample_weight
 
-        if y is not None and self.loss_fn is not None:
+        if y is not None and self.loss_fn is not None and training:
             y = ops.argmax(y, -1) if ops.ndim(y) > 1 else y  # type: ignore
 
             x_min = ops.min(x)
