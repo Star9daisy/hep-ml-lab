bd3ac89f6024d6307cc4e9556ebef51aebf3d01f
Star9daisy <star9daisy@outlook.com>
2024-01-19 08:17:29 +0000

    feat: Expose classes and functions to physics_object sub-module

--- hml/physics_objects/__init__.py
+++ hml/physics_objects/__init__.py
@@ -1,2 +1,6 @@
 from .collective import CollectivePhysicsObject
+from .collective import is_collective_physics_object
+from .nested import NestedPhysicsObject
+from .nested import is_nested_physics_object
 from .single import SinglePhysicsObject
+from .single import is_single_physics_object
