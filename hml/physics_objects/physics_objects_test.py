01fc76d6749f549eebb605e37495390f853e2868
Star9daisy <star9daisy@outlook.com>
2024-01-23 11:06:56 +0000

    refactor: get method retrieve class according to its name

--- hml/physics_objects/physics_objects_test.py
+++ hml/physics_objects/physics_objects_test.py
@@ -8,23 +8,10 @@ from .single import Single
 
 
 def test_get():
-    obj = get("Jet0")
-    assert isinstance(obj, Single)
-
-    for name in ["Jet:", "Jet1:", "Jet:3", "Jet1:3"]:
-        obj = get(name)
-        assert isinstance(obj, Collective)
-
-    for name in ["Jet1.Constituents2", "Jet1.Constituents:"]:
-        obj = get(name)
-        assert isinstance(obj, Nested)
-
-    for name in ["Jet1,Jet2", "Jet:.Constituents:,Jet0"]:
-        obj = get(name)
-        assert isinstance(obj, Multiple)
+    assert get("single") == Single
+    assert get("collective") == Collective
+    assert get("nested") == Nested
+    assert get("multiple") == Multiple
 
     with pytest.raises(ValueError):
-        get("Jet1.Constituents2.Constituents2")
-
-    with pytest.raises(ValueError):
-        get("Jet.Constituents")
+        assert get("unknown")
