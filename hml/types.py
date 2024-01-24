5fbda933928b8d4134c9e89159316be5ee824986
Star9daisy <star9daisy@outlook.com>
2024-01-24 08:01:55 +0000

    feat: Add new type "VariableInteger"

--- hml/types.py
+++ hml/types.py
@@ -1,8 +1,22 @@
 from pathlib import Path
-from typing import Literal, Union
+from typing import Literal
+from typing import Union
 
 PathLike = Union[str, Path]
 CutLayerTopologies = Literal["parallel", "sequential"]
+
+
+class VariableInteger(int):
+    """A variable integer that represents any integer."""
+
+    def __repr__(self):
+        return "var"
+
+    def __eq__(self, other: int):
+        if not isinstance(other, int):
+            raise TypeError(f"Cannot compare VariableInteger to {type(other)}")
+
+        return True
 
 
 class Generator:
