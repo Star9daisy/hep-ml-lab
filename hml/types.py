dd0d1a264dc8063adf074eb5d9421ce2fa563d57
Star9daisy <star9daisy@outlook.com>
2024-01-24 10:36:38 +0000

    chore: Remove VariableInteger type

--- hml/types.py
+++ hml/types.py
@@ -6,19 +6,6 @@ PathLike = Union[str, Path]
 CutLayerTopologies = Literal["parallel", "sequential"]
 
 
-class VariableInteger(int):
-    """A variable integer that represents any integer."""
-
-    def __repr__(self):
-        return "var"
-
-    def __eq__(self, other: int):
-        if not isinstance(other, int):
-            raise TypeError(f"Cannot compare VariableInteger to {type(other)}")
-
-        return True
-
-
 class Generator:
     ...
 
