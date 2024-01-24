d7574040d2a99c9ba6b9d3820f252659811654ac
Star9daisy <star9daisy@outlook.com>
2024-01-24 10:12:06 +0000

    feat: Add new observable AngularDistance

--- hml/observables/angular_distance.py
+++ hml/observables/angular_distance.py
@@ -1,66 +1,55 @@
-from ..types import Observable
-from ..utils import get_observable
+from typing import Any
+
+import awkward as ak
+import numpy as np
+
+from .azimuthal_angle import Phi
+from .observable import Observable
+from .pseudo_rapidity import Eta
 
 
 class AngularDistance(Observable):
-    def __init__(self, physics_object, sorted_by="pt", reverse=True, **kwargs):
-        super().__init__(physics_object, **kwargs)
-        self.sorted_by = sorted_by
-        self.reverse = reverse
-        self.orders = [
-            get_observable(f"{i}.{sorted_by}") for i in physics_object.split(",")
-        ]
-
-    def get_value(self):
-        if len(self.main_objs) != 2:
-            return
-
-        for i in self.orders:
-            i.read(self.event)
-
-        if len(self.sub_objs[0]) != 0:
-            flat_obj0 = [i for subs in self.sub_objs[0] for i in subs]
-            flat_order0 = [i for subs in self.orders[0].value for i in subs]
-        else:
-            flat_obj0 = [i for i in self.main_objs[0]]
-            flat_order0 = [i for i in self.orders[0].value]
-
-        if len(self.sub_objs[1]) != 0:
-            flat_obj1 = [i for subs in self.sub_objs[1] for i in subs]
-            flat_order1 = [i for subs in self.orders[1].value for i in subs]
-        else:
-            flat_obj1 = [i for i in self.main_objs[1]]
-            flat_order1 = [i for i in self.orders[1].value]
-
-        sorted_obj0 = [
-            obj
-            for _, obj in sorted(
-                zip(flat_order0, flat_obj0),
-                key=lambda pair: pair[0],
-                reverse=self.reverse,
+    def __init__(
+        self,
+        physics_object: str,
+        name: str | None = None,
+        value: Any = None,
+        dtype: Any = None,
+    ):
+        supported_objects = ["single", "collective", "nested", "multiple"]
+        super().__init__(physics_object, supported_objects, name, value, dtype)
+        if len(self.physics_object.all) != 2:
+            raise ValueError(
+                "AngularDistance requires exactly two physics objects, but got"
+                f" {len(self.physics_object.all)}"
             )
+
+    def read(self, event):
+        eta1 = Eta(self.physics_object.all[0].identifier).read(event)
+        eta2 = Eta(self.physics_object.all[1].identifier).read(event)
+        phi1 = Phi(self.physics_object.all[0].identifier).read(event)
+        phi2 = Phi(self.physics_object.all[1].identifier).read(event)
+
+        eta1_values = eta1.to_awkward()
+        eta1_values = ak.flatten(eta1_values, axis=None)
+
+        eta2_values = eta2.to_awkward()
+        eta2_values = ak.flatten(eta2_values, axis=None)
+
+        phi1_values = phi1.to_awkward()
+        phi1_values = ak.flatten(phi1_values, axis=None)
+
+        phi2_values = phi2.to_awkward()
+        phi2_values = ak.flatten(phi2_values, axis=None)
+
+        point1 = list(zip(eta1_values, phi1_values))
+        point2 = list(zip(eta2_values, phi2_values))
+
+        self._value = [
+            [np.hypot(x1 - x2, y1 - y2) for x2, y2 in point2] for x1, y1 in point1
         ]
-        sorted_obj1 = [
-            obj
-            for _, obj in sorted(
-                zip(flat_order1, flat_obj1),
-                key=lambda pair: pair[0],
-                reverse=self.reverse,
-            )
-        ]
 
-        values = []
-        for i in sorted_obj0:
-            distance_per_i = []
-
-            for j in sorted_obj1:
-                if i is None or j is None:
-                    distance_per_i.append(float("nan"))
-                else:
-                    distance_per_i.append(i.P4().DeltaR(j.P4()))
-            values.append(distance_per_i)
-
-        return values
+        return self
 
 
 class DeltaR(AngularDistance):
