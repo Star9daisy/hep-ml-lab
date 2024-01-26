da8cff386cd517f2f2cc11e8a7c7fec2101cee36
Star9daisy <star9daisy@outlook.com>
2024-01-26 14:30:14 +0000

    refactor: Update abstract class PhysicsObject

--- hml/physics_objects/physics_object.py
+++ hml/physics_objects/physics_object.py
@@ -3,8 +3,6 @@ from __future__ import annotations
 from abc import ABC
 from abc import abstractclassmethod
 from abc import abstractmethod
-from abc import abstractproperty
-from typing import Any
 
 
 class PhysicsObject(ABC):
@@ -12,24 +10,6 @@ class PhysicsObject(ABC):
     def read_ttree(self, ttree):
         ...
 
-    @abstractproperty
-    def id(self):
-        ...
-
     @abstractclassmethod
-    def from_id(cls, id: str):
+    def from_name(cls, name: str) -> PhysicsObject:
         ...
-
-    @abstractproperty
-    def config(self) -> dict[str, Any]:
-        ...
-
-    @abstractclassmethod
-    def from_config(cls, config: dict[str, Any]):
-        ...
-
-    def __repr__(self) -> str:
-        return f"{self.__class__.__name__}: {self.id}"
-
-    def __eq__(self, other: PhysicsObject) -> bool:
-        return self.config == other.config
