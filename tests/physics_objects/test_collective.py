eafaa4f3ab6ec062a22922978661a0a5250888ba
Star9daisy <star9daisy@outlook.com>
2024-02-02 07:38:44 +0000

    test: Update to the new physics objects

--- tests/physics_objects/test_collective.py
+++ tests/physics_objects/test_collective.py
@@ -1,3 +1,5 @@
+import pytest
+
 from hml.physics_objects.collective import Collective
 from hml.physics_objects.collective import is_collective
 
@@ -5,14 +7,12 @@ from hml.physics_objects.collective import is_collective
 def test_init():
     obj = Collective(branch="Jet", start=1, stop=3)
 
-    # Parameters ------------------------------------------------------------- #
+    # Attributes ------------------------------------------------------------- #
     assert obj.branch == "Jet"
     assert obj.start == 1
     assert obj.stop == 3
-
-    # Attributes ------------------------------------------------------------- #
     assert obj.name == "Jet1:3"
-    assert obj.value is None
+    assert obj.objects is None
     assert obj.config == {"branch": "Jet", "start": 1, "stop": 3}
 
     # other names
@@ -21,34 +21,56 @@ def test_init():
     assert Collective(branch="Jet", stop=3).name == "Jet:3"
 
 
+def test_special_methods():
+    obj = Collective(branch="Jet", start=1, stop=3)
+
+    # __eq__ ----------------------------------------------------------------- #
+    assert Collective(branch="Jet", start=1) == Collective(branch="Jet", start=1)
+
+    # __repr__ --------------------------------------------------------------- #
+    assert repr(obj) == "Collective(name='Jet1:3', objects=None)"
+
+
 def test_class_methods():
     obj = Collective(branch="Jet", start=1, stop=3)
-    assert repr(obj) == "Collective(name='Jet1:3', value=None)"
+
+    # from_name -------------------------------------------------------------- #
     assert obj == Collective.from_name("Jet1:3")
+
+    # from_config ------------------------------------------------------------ #
     assert obj == Collective.from_config(obj.config)
 
 
 def test_read_ttree(event):
-    # Valid cases ------------------------------------------------------------ #
+    # Common cases ----------------------------------------------------------- #
+    obj = Collective(branch="Jet").read_ttree(event)
+    assert len(obj.objects) > 0
+    assert None not in obj.objects
+
     obj = Collective(branch="Jet", stop=3).read_ttree(event)
-    assert all(obj.value) is True
+    assert len(obj.objects) == 3
+    assert None not in obj.objects
+
+    obj = Collective(branch="Jet", stop=10).read_ttree(event)
+    assert len(obj.objects) == 10
+    assert None in obj.objects
 
     obj = Collective(branch="Constituents", stop=3).read_ttree(event.Jet[0])
-    assert all(obj.value) is True
+    assert len(obj.objects) == 3
+    assert None not in obj.objects
 
-    # Invalid cases ---------------------------------------------------------- #
+    # Edge cases ------------------------------------------------------------- #
     obj = Collective(branch="Jet", start=100).read_ttree(event)
-    assert obj.value == []
+    assert len(obj.objects) == 0
+    assert obj.objects == []
 
     obj = Collective(branch="Jet", start=100, stop=102).read_ttree(event)
-    assert len(obj.value) == 2
-    assert obj.value == [None, None]
+    assert len(obj.objects) == 2
+    assert obj.objects == [None, None]
 
-    # None-filled cases
-    obj = Collective(branch="Jet", stop=100).read_ttree(event)
-    assert len(obj.value) == 100
-    assert any(obj.value) is True
-    assert all(obj.value) is False
+    # Error cases ------------------------------------------------------------ #
+    with pytest.raises(ValueError):
+        Collective(branch="Unknown").read_ttree(event)
 
 
 def test_is_collective(single_names, collective_names, nested_names, multiple_names):
