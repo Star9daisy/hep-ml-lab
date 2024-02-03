3354164a4fe6ffaceb44f2fe96154b206fdeace2
Star9daisy <star9daisy@outlook.com>
2024-02-03 13:10:57 +0000

    config: Expose modules at the highest level

--- hml/__init__.py
+++ hml/__init__.py
@@ -1,16 +1,13 @@
-# Import fastjet first, otherwise it will raise a segmentation fault.
-# import fastjet
+import fastjet
 
-# from hml import approaches
-# from hml import datasets
-# from hml import events
-
-# from hml import generators
-# from hml import metrics
-# from hml import observables
-# from hml import representations
-# from hml import types
-# from hml import utils
-# from hml.version import __version__
+from hml import approaches
+from hml import datasets
+from hml import events
+from hml import generators
+from hml import metrics
+from hml import observables
+from hml import representations
+from hml import types
+from hml import utils
 
 __version__ = "0.3.1"
