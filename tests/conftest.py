84513a1c711b63f40315d5fed6dc728811e710e3
Star9daisy <star9daisy@outlook.com>
2024-03-03 10:50:59 +0000

    test: Remove PyROOT-related fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,20 @@
-import pytest
-import ROOT
+# import pytest
+# import ROOT
 
-from hml.generators.madgraph5 import Madgraph5Run
-
-
-@pytest.fixture
-def event():
-    ROOT.gSystem.Load("libDelphes")
-    filepath = "tests/data/pp2zz/Events/run_01/tag_1_delphes_events.root"
-    file = ROOT.TFile(filepath)
-    tree = file.Get("Delphes")
-    tree.GetEntry(0)
-
-    yield tree
+# from hml.generators.madgraph5 import Madgraph5Run
 
 
-@pytest.fixture
-def run():
-    return Madgraph5Run("tests/data/pp2zz", "run_01")
+# @pytest.fixture
+# def event():
+#     ROOT.gSystem.Load("libDelphes")
+#     filepath = "tests/data/pp2zz/Events/run_01/tag_1_delphes_events.root"
+#     file = ROOT.TFile(filepath)
+#     tree = file.Get("Delphes")
+#     tree.GetEntry(0)
+
+#     yield tree
+
+
+# @pytest.fixture
+# def run():
+#     return Madgraph5Run("tests/data/pp2zz", "run_01")
