922a665b2141917d5d5c30f4a44b37a2e664b52f
Star9daisy <star9daisy@outlook.com>
2024-03-03 09:27:37 +0000

    chore: Remove legacy modules

--- hml/__init__.py
+++ hml/__init__.py
@@ -2,12 +2,9 @@ import fastjet
 
 from hml import approaches
 from hml import datasets
-from hml import events
 from hml import generators
 from hml import metrics
 from hml import observables
 from hml import representations
-from hml import types
-from hml import utils
 
 __version__ = "0.4.0"
