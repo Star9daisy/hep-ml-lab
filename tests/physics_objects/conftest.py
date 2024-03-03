4521099152fcb326da390bc25ada54ccfbb761a7
Star9daisy <star9daisy@outlook.com>
2024-03-03 11:16:38 +0000

    test: Update fixture for physics object names

--- tests/physics_objects/conftest.py
+++ tests/physics_objects/conftest.py
@@ -4,53 +4,43 @@ import pytest
 @pytest.fixture
 def single_names():
     yield [
-        "Jet0",
+        "jet0",
     ]
 
 
 @pytest.fixture
 def collective_names():
     yield [
-        "Jet:",
-        "Jet1:",
-        "Jet:3",
-        "Jet1:3",
+        "jet",
+        "jet:",
+        "jet1:",
+        "jet:3",
+        "jet1:3",
     ]
 
 
 @pytest.fixture
 def nested_names():
     yield [
-        "Jet0.Constituents0",
-        "Jet0.Constituents:",
-        "Jet0.Constituents1:",
-        "Jet0.Constituents:3",
-        "Jet0.Constituents1:3",
-        "Jet:.Constituents:",
-        "Jet:.Constituents1:",
-        "Jet:.Constituents:3",
-        "Jet:.Constituents1:3",
-        "Jet1:.Constituents:",
-        "Jet1:.Constituents1:",
-        "Jet1:.Constituents:3",
-        "Jet1:.Constituents1:3",
-        "Jet:3.Constituents:",
-        "Jet:3.Constituents1:",
-        "Jet:3.Constituents:3",
-        "Jet:3.Constituents1:3",
-        "Jet1:3.Constituents:",
-        "Jet1:3.Constituents1:",
-        "Jet1:3.Constituents:3",
-        "Jet1:3.Constituents1:3",
-    ]
-
-
-@pytest.fixture
-def multiple_names():
-    yield [
-        "Jet0,Jet1",
-        "Jet0,Jet1,Jet2",
-        "Jet0,Jet:",
-        "Jet0,Jet0.Constituents:",
-        "Jet0.Constituents:,Jet0.Constituents:",
+        "jet0.Constituents0",
+        "jet0.Constituents:",
+        "jet0.Constituents1:",
+        "jet0.Constituents:3",
+        "jet0.Constituents1:3",
+        "jet:.Constituents:",
+        "jet:.Constituents1:",
+        "jet:.Constituents:3",
+        "jet:.Constituents1:3",
+        "jet1:.Constituents:",
+        "jet1:.Constituents1:",
+        "jet1:.Constituents:3",
+        "jet1:.Constituents1:3",
+        "jet:3.Constituents:",
+        "jet:3.Constituents1:",
+        "jet:3.Constituents:3",
+        "jet:3.Constituents1:3",
+        "jet1:3.Constituents:",
+        "jet1:3.Constituents1:",
+        "jet1:3.Constituents:3",
+        "jet1:3.Constituents1:3",
     ]
