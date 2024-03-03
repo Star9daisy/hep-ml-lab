7d57d9d091503a0e29c0a11b80e20f87623fd63b
Star9daisy <star9daisy@outlook.com>
2024-03-03 11:13:22 +0000

    test: Add tests for physics objects

--- tests/physics_objects/test_collective.py
+++ tests/physics_objects/test_collective.py
@@ -1,89 +1,52 @@
 import pytest
 
-from hml.physics_objects.collective import Collective
-from hml.physics_objects.collective import is_collective
+from hml.physics_objects.collective import Collective, is_collective
 
 
 def test_init():
-    obj = Collective(branch="Jet", start=1, stop=3)
+    obj = Collective(branch="jet", start=1, stop=3)
 
-    # Attributes ------------------------------------------------------------- #
-    assert obj.branch == "Jet"
     assert obj.start == 1
     assert obj.stop == 3
-    assert obj.name == "Jet1:3"
-    assert obj.objects is None
-    assert obj.config == {"branch": "Jet", "start": 1, "stop": 3}
+
+    assert obj.name == "jet1:3"
+    assert obj.branch == "jet"
+    assert obj.slices == [slice(1, 3)]
+    assert obj.config == {"branch": "jet", "start": 1, "stop": 3}
 
     # other names
-    assert Collective(branch="Jet").name == "Jet:"
-    assert Collective(branch="Jet", start=1).name == "Jet1:"
-    assert Collective(branch="Jet", stop=3).name == "Jet:3"
+    assert Collective(branch="jet").name == "jet"
+    assert Collective(branch="jet", start=1).name == "jet1:"
+    assert Collective(branch="jet", stop=3).name == "jet:3"
 
 
 def test_special_methods():
-    obj = Collective(branch="Jet", start=1, stop=3)
+    obj = Collective(branch="jet", start=1, stop=3)
 
-    # __eq__ ----------------------------------------------------------------- #
-    assert Collective(branch="Jet", start=1) == Collective(branch="Jet", start=1)
-
-    # __repr__ --------------------------------------------------------------- #
-    assert repr(obj) == "Collective(name='Jet1:3', objects=None)"
+    assert obj == Collective(branch="jet", start=1, stop=3)
+    assert repr(obj) == "Collective(branch='jet', start=1, stop=3)"
 
 
 def test_class_methods():
-    obj = Collective(branch="Jet", start=1, stop=3)
+    obj = Collective(branch="jet", start=1, stop=3)
 
-    # from_name -------------------------------------------------------------- #
-    assert obj == Collective.from_name("Jet1:3")
-
-    # from_config ------------------------------------------------------------ #
+    assert obj == Collective.from_name("jet1:3")
     assert obj == Collective.from_config(obj.config)
 
-
-def test_read_ttree(event):
-    # Common cases ----------------------------------------------------------- #
-    obj = Collective(branch="Jet").read_ttree(event)
-    assert len(obj.objects) > 0
-    assert None not in obj.objects
-
-    obj = Collective(branch="Jet", stop=3).read_ttree(event)
-    assert len(obj.objects) == 3
-    assert None not in obj.objects
-
-    obj = Collective(branch="Jet", stop=10).read_ttree(event)
-    assert len(obj.objects) == 10
-    assert None in obj.objects
-
-    obj = Collective(branch="Constituents", stop=3).read_ttree(event.Jet[0])
-    assert len(obj.objects) == 3
-    assert None not in obj.objects
-
-    # Edge cases ------------------------------------------------------------- #
-    obj = Collective(branch="Jet", start=100).read_ttree(event)
-    assert len(obj.objects) == 0
-    assert obj.objects == []
-
-    obj = Collective(branch="Jet", start=100, stop=102).read_ttree(event)
-    assert len(obj.objects) == 2
-    assert obj.objects == [None, None]
-
-    # Error cases ------------------------------------------------------------ #
     with pytest.raises(ValueError):
-        Collective(branch="Unknown").read_ttree(event)
+        Collective.from_name("jet0")
 
 
-def test_is_collective(single_names, collective_names, nested_names, multiple_names):
-    # Valid cases ------------------------------------------------------------ #
+def test_is_collective(single_names, collective_names, nested_names):
+    assert is_collective(None) is False
+
+    assert is_collective(Collective(branch="jet", start=1, stop=3)) is True
+
     for case in collective_names:
         assert is_collective(case) is True
 
-    # Invalid cases ---------------------------------------------------------- #
     for case in single_names:
         assert is_collective(case) is False
 
     for case in nested_names:
-        assert is_collective(case) is False
-
-    for case in multiple_names:
         assert is_collective(case) is False
--- tests/physics_objects/test_nested.py
+++ tests/physics_objects/test_nested.py
@@ -1,158 +1,47 @@
-from hml.physics_objects import Collective
-from hml.physics_objects import Nested
-from hml.physics_objects import Single
-from hml.physics_objects import is_nested
+import pytest
+
+from hml.physics_objects import Collective, Nested, Single, is_nested
 
 
 def test_init():
-    obj = Nested(
-        main=Single(branch="Jet", index=0),
-        sub=Collective(branch="Constituents", start=1, stop=3),
-    )
+    obj = Nested(main="jet0", sub="constituents1:3")
 
-    # Attributes ------------------------------------------------------------- #
-    assert obj.main.branch == "Jet"
-    assert obj.main.index == 0
+    assert obj.main == Single(branch="jet", index=0)
+    assert obj.sub == Collective(branch="constituents", start=1, stop=3)
 
-    assert obj.sub.branch == "Constituents"
-    assert obj.sub.start == 1
-    assert obj.sub.stop == 3
-
-    assert obj.name == "Jet0.Constituents1:3"
-    assert obj.objects is None
-    assert obj.config == {
-        "main": {
-            "class_name": "Single",
-            "config": {"branch": "Jet", "index": 0},
-        },
-        "sub": {
-            "class_name": "Collective",
-            "config": {
-                "branch": "Constituents",
-                "start": 1,
-                "stop": 3,
-            },
-        },
-    }
+    assert obj.name == "jet0.constituents1:3"
+    assert obj.branch == "jet.constituents"
+    assert obj.slices == [slice(0, 1), slice(1, 3)]
+    assert obj.config == {"main": "jet0", "sub": "constituents1:3"}
 
 
 def test_special_methods():
-    obj = Nested(
-        main=Single(branch="Jet", index=0),
-        sub=Collective(branch="Constituents", start=1, stop=3),
-    )
+    obj = Nested(main="jet0", sub="constituents1:3")
 
-    # __eq__ ----------------------------------------------------------------- #
-    assert (
-        Nested(
-            main=Single(branch="Jet", index=0),
-            sub=Collective(branch="Constituents", start=1, stop=3),
-        )
-        == obj
-    )
-
-    # __repr__ --------------------------------------------------------------- #
-    assert repr(obj) == "Nested(name='Jet0.Constituents1:3', objects=None)"
+    assert obj == Nested(main="jet0", sub="constituents1:3")
+    assert repr(obj) == "Nested(main='jet0', sub='constituents1:3')"
 
 
 def test_class_methods():
-    obj = Nested(
-        main=Single(branch="Jet", index=0),
-        sub=Collective(branch="Constituents", start=1, stop=3),
-    )
+    obj = Nested(main="jet0", sub="constituents1:3")
 
-    # from_name -------------------------------------------------------------- #
-    assert obj == Nested.from_name("Jet0.Constituents1:3")
-
-    # from_config ------------------------------------------------------------ #
+    assert obj == Nested.from_name("jet0.constituents1:3")
     assert obj == Nested.from_config(obj.config)
 
-
-def test_read_ttree(event):
-    # Common cases ----------------------------------------------------------- #
-    obj = Nested.from_name("Jet0.Constituents0").read_ttree(event)
-    assert (len(obj.objects), len(obj.objects[0])) == (1, 1)
-    assert None not in obj.objects[0]
-
-    obj = Nested.from_name("Jet0.Constituents:3").read_ttree(event)
-    assert (len(obj.objects), len(obj.objects[0])) == (1, 3)
-    assert None not in obj.objects[0]
-
-    obj = Nested.from_name("Jet:3.Constituents:3").read_ttree(event)
-    assert (len(obj.objects), len(obj.objects[0])) == (3, 3)
-    assert None not in [j for i in obj.objects for j in i]
-
-    # Edge cases ------------------------------------------------------------- #
-    obj = Nested.from_name("Jet0.Constituents100").read_ttree(event)
-    assert (len(obj.objects), len(obj.objects[0])) == (1, 0)
-    assert obj.objects == [[]]
-
-    # None-filled cases
-    obj = Nested.from_name("Jet0.Constituents:100").read_ttree(event)
-    assert (len(obj.objects), len(obj.objects[0])) == (1, 100)
-    assert all([j for i in obj.objects for j in i]) is False
-    assert any([j for i in obj.objects for j in i]) is True
-
-    obj = Nested.from_name("Jet0.Constituents100:").read_ttree(event)
-    assert (len(obj.objects), len(obj.objects[0])) == (1, 0)
-    assert obj.objects == [[]]
-
-    obj = Nested.from_name("Jet100.Constituents0").read_ttree(event)
-    assert obj.objects == []
-
-    obj = Nested.from_name("Jet100.Constituents100").read_ttree(event)
-    assert obj.objects == []
-
-    obj = Nested.from_name("Jet100.Constituents:100").read_ttree(event)
-    assert obj.objects == []
-
-    obj = Nested.from_name("Jet100.Constituents100:").read_ttree(event)
-    assert obj.objects == []
-
-    # None-filled cases
-    obj = Nested.from_name("Jet:100.Constituents0").read_ttree(event)
-    assert (len(obj.objects), len(obj.objects[0])) == (100, 1)
-    for i in obj.objects:
-        assert len(i) in [0, 1]
-
-    obj = Nested.from_name("Jet:100.Constituents100").read_ttree(event)
-    assert (len(obj.objects), len(obj.objects[0])) == (100, 0)
-    assert [j for i in obj.objects for j in i] == []
-
-    # None-filled cases
-    obj = Nested.from_name("Jet:100.Constituents:100").read_ttree(event)
-    assert (len(obj.objects), len(obj.objects[0])) == (100, 100)
-    assert all([j for i in obj.objects for j in i]) is False
-    assert any([j for i in obj.objects for j in i]) is True
-
-    obj = Nested.from_name("Jet:100.Constituents100:").read_ttree(event)
-    assert (len(obj.objects), len(obj.objects[0])) == (100, 0)
-    assert [j for i in obj.objects for j in i] == []
-
-    obj = Nested.from_name("Jet100:.Constituents0").read_ttree(event)
-    assert obj.objects == []
-
-    obj = Nested.from_name("Jet100:.Constituents100").read_ttree(event)
-    assert obj.objects == []
-
-    obj = Nested.from_name("Jet100:.Constituents:100").read_ttree(event)
-    assert obj.objects == []
-
-    obj = Nested.from_name("Jet100:.Constituents100:").read_ttree(event)
-    assert obj.objects == []
+    with pytest.raises(ValueError):
+        Nested.from_name("jet0")
 
 
-def test_is_nested(single_names, collective_names, nested_names, multiple_names):
-    # Valid cases ------------------------------------------------------------ #
+def test_is_nested(single_names, collective_names, nested_names):
+    assert is_nested(None) is False
+
+    assert is_nested(Nested(main="jet0", sub="constituents1:3")) is True
+
     for case in nested_names:
         assert is_nested(case) is True
 
-    # Invalid cases ---------------------------------------------------------- #
     for case in single_names:
         assert is_nested(case) is False
 
     for case in collective_names:
-        assert is_nested(case) is False
-
-    for case in multiple_names:
         assert is_nested(case) is False
