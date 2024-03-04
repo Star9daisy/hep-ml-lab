b74ae5732b9410433a57f6b8e8de31e666c1d3fc
Star9daisy <star9daisy@outlook.com>
2024-03-04 07:19:04 +0000

    test: Add a fixture of uproot events

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,10 @@
-# import pytest
-# import ROOT
-
-# from hml.generators.madgraph5 import Madgraph5Run
+import pytest
+import uproot
 
 
-# @pytest.fixture
-# def event():
-#     ROOT.gSystem.Load("libDelphes")
-#     filepath = "tests/data/pp2zz/Events/run_01/tag_1_delphes_events.root"
-#     file = ROOT.TFile(filepath)
-#     tree = file.Get("Delphes")
-#     tree.GetEntry(0)
+@pytest.fixture
+def events():
+    filepath = "tests/data/pp2zz/Events/run_01/tag_1_delphes_events.root"
+    events = uproot.open(filepath)["Delphes"]
 
-#     yield tree
-
-
-# @pytest.fixture
-# def run():
-#     return Madgraph5Run("tests/data/pp2zz", "run_01")
+    yield events
