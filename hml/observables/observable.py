3a5ba6de6fdf5745403ba42391819f623f81bac6
Star9daisy <star9daisy@outlook.com>
2024-03-04 07:17:56 +0000

    refactor: Brand new observable supports for uproot events

--- hml/observables/observable.py
+++ hml/observables/observable.py
@@ -1,132 +1,156 @@
 from __future__ import annotations
 
-from math import nan
-from typing import Any
-
 import awkward as ak
 import numpy as np
 
-from ..physics_objects import ALL_OBJECTS_DICT
-from ..physics_objects import get_physics_object
-from ..physics_objects.multiple import is_multiple
+from .. import physics_objects
+from ..physics_objects import PhysicsObject
+from .observable_utils import branches_to_momentum4d, get_constituents
+
+ALL_OBJECTS_DICT = {}
 
 
 class Observable:
-    ALL_OBSERVABLES = {}
-
     def __init__(
         self,
-        physics_object: str | None = None,
-        supported_types: list[str] | None = None,
-    ):
-        self._validate_physics_object(physics_object, supported_types)
+        physics_object: str | PhysicsObject | None = None,
+        class_name: str | None = None,
+    ) -> None:
+        self.physics_object = physics_object
+        self.class_name = class_name
 
-        self.physics_object = get_physics_object(physics_object)
-        self.supported_types = supported_types
+        self.value = None
 
     def __init_subclass__(cls, **kwargs) -> None:
         super().__init_subclass__(**kwargs)
-        Observable.ALL_OBSERVABLES[cls.__name__] = cls
+        ALL_OBJECTS_DICT[cls.__name__] = cls
+
+    def __eq__(self, other: str | Observable) -> bool:
+        if isinstance(other, str):
+            other = self.from_name(other)
+
+        return (
+            self.physics_object == other.physics_object
+            and self.__class__ == other.__class__
+        )
+
+    def __str__(self) -> str:
+        return self.name
 
     def __repr__(self) -> str:
-        return f"{self.name} : {self.value}"
-
-    def __eq__(self, other) -> bool:
-        return self.config == other.config
-
-    def read_ttree(self, entry) -> Any:
-        raise NotImplementedError
-
-    def to_awkward(self, dtype=None):
-        value = self.value if isinstance(self.value, list) else [self.value]
-
-        ak_array = ak.from_iter(value)
-        ak_array = ak.values_astype(ak_array, dtype)
-        try:
-            ak_array = ak.to_regular(ak_array)
-        except ValueError:
-            pass
-        return ak_array
-
-    def to_numpy(self, squeeze=True, dtype=None):
-        value = self.value if isinstance(self.value, list) else [self.value]
-        if squeeze:
-            return np.squeeze(np.array(value, dtype=dtype))
-        else:
-            return np.array(value, dtype=dtype)
-
-    @property
-    def name(self):
-        if self.physics_object is not None:
-            return f"{self.physics_object.name}.{self.__class__.__name__}"
-        else:
-            return self.__class__.__name__
-
-    @property
-    def value(self):
-        if hasattr(self, "_value"):
-            return self._value
-        else:
-            return nan
-
-    @property
-    def shape(self):
-        return str(self.to_awkward().type)
-
-    @property
-    def config(self) -> dict[str, Any]:
-        if self.physics_object is not None:
-            return {"physics_object": self.physics_object.name}
-        else:
-            return {}
-
-    @classmethod
-    def from_name(cls, name: str, **kwargs) -> Observable:
-        if "." in name:
-            physics_object = name.split(".")[0]
-        else:
-            physics_object = None
-
-        return cls(physics_object, **kwargs)
-
-    @classmethod
-    def from_config(cls, config: dict[str, Any]) -> Observable:
-        return cls(**config)
+        return f"{self.name}: {self.value.type!s}"
 
     @classmethod
     def add_alias(cls, *alias: str) -> None:
         for i in alias:
-            Observable.ALL_OBSERVABLES[i] = cls
+            ALL_OBJECTS_DICT[i] = cls
 
-    def _validate_physics_object(
-        self,
-        name: str | None,
-        supported_types: list[str] | None,
-    ) -> bool:
-        if supported_types is None:
-            return
+    @classmethod
+    def from_name(cls, name: str, **kwargs) -> "Observable":
+        *parts, class_name = name.split(".")
 
-        if name is None and supported_types is not None:
+        if class_name not in ALL_OBJECTS_DICT:
             raise ValueError(
-                "If supported_types is provided, physics_object must also be provided."
+                f"Invalid class name '{class_name}' for the observable {cls.__name__}"
+            )
+        elif ALL_OBJECTS_DICT[class_name] != cls:
+            raise ValueError(
+                f"Invalid class name '{class_name}' for the observable {cls.__name__}"
             )
 
-        # Avoid the remove method to modify the original list
-        supported_types = supported_types.copy()
+        physics_object = ".".join(parts) if len(parts) > 0 else None
 
-        if "multiple" in supported_types:
-            supported_types.remove("multiple")
+        return cls(physics_object, class_name, **kwargs)
 
-            if not is_multiple(name, supported_types):
-                raise ValueError(
-                    f"{name} is not a valid identifier for a multiple physics object."
-                )
+    @classmethod
+    def from_config(cls, config: dict) -> "Observable":
+        return cls(**config)
+
+    @property
+    def physics_object(self) -> PhysicsObject | None:
+        return self._physics_object
+
+    @physics_object.setter
+    def physics_object(self, physics_object: str | PhysicsObject | None) -> None:
+        if isinstance(physics_object, PhysicsObject):
+            self._physics_object = physics_object
+        else:
+            self._physics_object = physics_objects.parse(physics_object)
+
+    @property
+    def class_name(self) -> str:
+        return self._class_name
+
+    @class_name.setter
+    def class_name(self, class_name: str | None) -> None:
+        self._class_name = class_name or self.__class__.__name__
+
+    @property
+    def name(self) -> str:
+        if self.physics_object is None:
+            return self.class_name
+
+        return f"{self.physics_object.name}.{self.class_name}"
+
+    @property
+    def value(self) -> ak.Array:
+        try:
+            return ak.to_regular(self._value, axis=None)
+
+        except Exception:
+            return self._value
+
+    @value.setter
+    def value(self, value: ak.Array | None) -> None:
+        self._value = value or ak.Array([])
+
+    @property
+    def config(self) -> dict:
+        return {
+            "physics_object": str(self.physics_object),
+            "class_name": self.class_name,
+        }
+
+    def read(self, events) -> "Observable":
+        if self.physics_object is None:
+            raise ValueError("No physics object specified")
+
+        all_branches = {key.lower(): key for key in events.keys(recursive=False)}
+        all_keys = {key.lower(): key for key in events.keys(full_paths=False)}
+
+        branch = self.physics_object.branch.lower()
+        slices = self.physics_object.slices
+        observable = self.__class__.__name__.lower()
+
+        if f"{branch}.{observable}" in all_keys:
+            key = all_keys[f"{branch}.{observable}"]
+            array = events[key].array()
+            value = array[:, *slices]
+
+        elif "constituents" in branch:
+            constituents = get_constituents(events, all_keys[branch])
+            value = getattr(constituents, observable)[:, *slices]
 
         else:
-            obj = get_physics_object(name)
-            supported_classes = [ALL_OBJECTS_DICT[i] for i in supported_types]
-            if type(obj) not in supported_classes:
-                raise ValueError(
-                    f"Physics object {obj} is not supported."
-                    f"It is type {type(obj)} but should be one of {supported_types}"
-                )
+            momentum4d = branches_to_momentum4d(events, all_branches[branch])
+            value = getattr(momentum4d, observable)[:, *slices]
+            print(str(value.type))
+
+        for i, slice_ in enumerate(slices):
+            if slice_.stop is not None:
+                start = slice_.start if slice_.start is not None else 0
+                required_length = slice_.stop - start
+
+                if i + 1 == len(slices):
+                    value = ak.pad_none(value, required_length, axis=i + 1)
+                    nan_float32 = np.array([np.nan], dtype="float32")[0]
+                    value = ak.fill_none(value, nan_float32)
+
+                else:
+                    n_missing = required_length - ak.num(value, i + 1)
+                    pad = ak.unflatten(ak.Array([[]] * ak.sum(n_missing)), n_missing)
+                    value = ak.concatenate([value, pad], axis=i + 1)
+
+        self._value = value
+
+        return self
---
+++ hml/observables/observable_utils.py
@@ -0,0 +1,138 @@
+import awkward as ak
+import numba as nb
+import vector
+
+vector.register_awkward()
+
+
+@nb.njit
+def find_1d_in_2d(a, b):  # pragma: no cover
+    index_array = []
+
+    for record_a, record_b in zip(a, b):
+        indices_per_a = []
+
+        for i in range(len(record_b)):
+            indices_per_b = []
+            for j in range(len(record_b[i])):
+                for k in range(len(record_a)):
+                    if record_b[i][j] == record_a[k]:
+                        indices_per_b.append(k)
+
+            indices_per_a.append(indices_per_b)
+        index_array.append(indices_per_a)
+
+    return index_array
+
+
+def branches_to_momentum4d(events, branch, with_id=False):
+    eta = events[f"{branch}.Eta"].array()
+    phi = events[f"{branch}.Phi"].array()
+
+    if f"{branch}.PT" in events:
+        pt = events[f"{branch}.PT"].array()
+        mass = events[f"{branch}.Mass"].array()
+    else:
+        pt = events[f"{branch}.ET"].array()
+        mass = ak.zeros_like(pt)
+
+    if with_id:
+        id_ = events[f"{branch}.fUniqueID"].array()
+        momenta = ak.zip(
+            {
+                "id": id_,
+                "pt": pt,
+                "eta": eta,
+                "phi": phi,
+                "mass": mass,
+            },
+            with_name="Momentum4D",
+        )
+        momenta["pt"] = ak.values_astype(momenta["pt"], "float32")
+        momenta["eta"] = ak.values_astype(momenta["eta"], "float32")
+        momenta["phi"] = ak.values_astype(momenta["phi"], "float32")
+        momenta["mass"] = ak.values_astype(momenta["mass"], "float32")
+
+    else:
+        momenta = ak.zip(
+            {
+                "pt": pt,
+                "eta": eta,
+                "phi": phi,
+                "mass": mass,
+            },
+            with_name="Momentum4D",
+        )
+        momenta = ak.values_astype(momenta, "float32")
+
+    return momenta
+
+
+@nb.njit
+def take_2d_from_1d(array, indices):  # pragma: no cover
+    pt = []
+    eta = []
+    phi = []
+    mass = []
+
+    for record, indices_at_1 in zip(array, indices):
+        pt.append(
+            [[record[i].pt for i in indices_at_2] for indices_at_2 in indices_at_1]
+        )
+        phi.append(
+            [[record[i].phi for i in indices_at_2] for indices_at_2 in indices_at_1]
+        )
+        eta.append(
+            [[record[i].eta for i in indices_at_2] for indices_at_2 in indices_at_1]
+        )
+        mass.append(
+            [[record[i].mass for i in indices_at_2] for indices_at_2 in indices_at_1]
+        )
+
+    return pt, eta, phi, mass
+
+
+def get_constituents(events, branch):
+    refs = events[branch].array()["refs"]
+    tracks = branches_to_momentum4d(events, "EFlowTrack", with_id=True)
+    photons = branches_to_momentum4d(events, "EFlowPhoton", with_id=True)
+    neutrals = branches_to_momentum4d(events, "EFlowNeutralHadron", with_id=True)
+
+    matched_tracks = ak.zip(
+        dict(
+            zip(
+                ["pt", "eta", "phi", "mass"],
+                take_2d_from_1d(tracks, ak.from_iter(find_1d_in_2d(tracks.id, refs))),
+            )
+        ),
+        with_name="Momentum4D",
+    )
+
+    matched_photons = ak.zip(
+        dict(
+            zip(
+                ["pt", "eta", "phi", "mass"],
+                take_2d_from_1d(photons, ak.from_iter(find_1d_in_2d(photons.id, refs))),
+            )
+        ),
+        with_name="Momentum4D",
+    )
+
+    matched_neutrals = ak.zip(
+        dict(
+            zip(
+                ["pt", "eta", "phi", "mass"],
+                take_2d_from_1d(
+                    neutrals, ak.from_iter(find_1d_in_2d(neutrals.id, refs))
+                ),
+            )
+        ),
+        with_name="Momentum4D",
+    )
+
+    constituents = ak.concatenate(
+        [matched_tracks, matched_photons, matched_neutrals], axis=-1
+    )
+    constituents = ak.values_astype(constituents, "float32")
+
+    return constituents
