2cfeaeab30c3a63c49932030740acff047df2910
Star9daisy <star9daisy@outlook.com>
2024-03-07 07:15:10 +0000

    refactor: Update physics object "single"

--- hml/physics_objects/single.py
+++ hml/physics_objects/single.py
@@ -1,34 +1,25 @@
+from __future__ import annotations
+
 import re
 
 from .physics_object import PhysicsObject
 
 
 def is_single(object_: str | PhysicsObject | None) -> bool:
-    if object_ is None:
-        return False
+    if isinstance(object_, str):
+        return bool(re.match(r"^[a-zA-Z]+\d+$", object_))
 
     elif isinstance(object_, PhysicsObject):
         return isinstance(object_, Single)
 
     else:
-        return bool(re.match(r"^[a-zA-Z]+\d+$", object_))
+        return False
 
 
 class Single(PhysicsObject):
     def __init__(self, branch: str, index: int) -> None:
         self.branch = branch
         self.index = index
-
-    @classmethod
-    def from_name(cls, name: str) -> "Single":
-        if (match_ := re.match(r"^([a-zA-Z]+)(\d+)$", name)) is None:
-            raise ValueError(
-                f"Invalid name '{name}' for a {cls.__name__} physics object"
-            )
-
-        branch, index = match_.groups()
-
-        return cls(branch, int(index))
 
     @property
     def branch(self) -> str:
@@ -44,19 +35,20 @@ class Single(PhysicsObject):
 
     @index.setter
     def index(self, index: int) -> None:
-        self._index = int(index)
+        self._index = index
 
     @property
     def name(self) -> str:
-        return self.branch + str(self.index)
+        return f"{self.branch}{self.index}"
 
-    @property
-    def slices(self) -> list[slice]:
-        return [slice(self.index, self.index + 1)]
+    @classmethod
+    def from_name(cls, name: str) -> Single:
+        if match_ := re.match(r"^([a-zA-Z]+)(\d+)$", name.strip()):
+            branch, index = match_.groups()
+            return cls(branch, int(index))
+
+        raise ValueError("Invalid name")
 
     @property
     def config(self) -> dict:
         return {"branch": self.branch, "index": self.index}
-
-
-Single.add_alias("single")
