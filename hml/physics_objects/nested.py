106d54f1ea0d70baa370fbeaf91e05111ada2c68
Star9daisy <star9daisy@outlook.com>
2024-03-07 07:48:45 +0000

    refactor: Update physics object "nested"

--- hml/physics_objects/nested.py
+++ hml/physics_objects/nested.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import re
 
 from .collective import Collective
@@ -6,14 +8,14 @@ from .single import Single, is_single
 
 
 def is_nested(object_: str | PhysicsObject | None) -> bool:
-    if object_ is None:
-        return False
+    if isinstance(object_, str):
+        return bool(re.match(r"^[a-zA-Z]+\d*:?\d*\.[a-zA-Z]+\d*:?\d*$", object_))
 
     elif isinstance(object_, PhysicsObject):
         return isinstance(object_, Nested)
 
     else:
-        return bool(re.match(r"^[a-zA-Z]+\d*:?\d*\.[a-zA-Z]+\d*:?\d*$", object_))
+        return False
 
 
 class Nested(PhysicsObject):
@@ -22,52 +24,47 @@ class Nested(PhysicsObject):
         main: str | PhysicsObject,
         sub: str | PhysicsObject,
     ) -> None:
-        self.main = main
-        self.sub = sub
+        self._main = self._init_object(main)
+        self._sub = self._init_object(sub)
 
-    @classmethod
-    def from_name(cls, name: str) -> "Nested":
-        main, sub = name.split(".")
+    def _init_object(self, object_: str | PhysicsObject) -> PhysicsObject:
+        if isinstance(object_, PhysicsObject):
+            return object_
 
-        return cls(main, sub)
+        elif is_single(object_):
+            return Single.from_name(object_)
+
+        else:
+            return Collective.from_name(object_)
 
     @property
     def main(self) -> PhysicsObject:
         return self._main
 
-    @main.setter
-    def main(self, main: str | PhysicsObject) -> None:
-        if is_single(main):
-            self._main = Single.from_name(main) if isinstance(main, str) else main
-        else:
-            self._main = Collective.from_name(main) if isinstance(main, str) else main
-
     @property
     def sub(self) -> PhysicsObject:
         return self._sub
 
-    @sub.setter
-    def sub(self, sub: str | PhysicsObject) -> None:
-        if is_single(sub):
-            self._sub = Single.from_name(sub) if isinstance(sub, str) else sub
-        else:
-            self._sub = Collective.from_name(sub) if isinstance(sub, str) else sub
-
     @property
     def branch(self) -> str:
-        return self.main.branch + "." + self.sub.branch
-
-    @property
-    def name(self) -> str:
-        return self.main.name + "." + self.sub.name
+        return f"{self.main.branch}.{self.sub.branch}"
 
     @property
     def slices(self) -> list[slice]:
         return [*self.main.slices, *self.sub.slices]
 
     @property
+    def name(self) -> str:
+        return f"{self.main.name}.{self.sub.name}"
+
+    @classmethod
+    def from_name(cls, name: str) -> Nested:
+        if "." in name:
+            main, sub = name.split(".")
+            return cls(main, sub)
+
+        raise ValueError
+
+    @property
     def config(self) -> dict:
         return {"main": self.main.name, "sub": self.sub.name}
-
-
-Nested.add_alias("nested")
