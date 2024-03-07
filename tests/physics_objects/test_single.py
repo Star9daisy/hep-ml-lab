71dface2ea218f66df1cf5d9f1a4f9d26c32c3d0
Star9daisy <star9daisy@outlook.com>
2024-03-07 07:15:10 +0000

    refactor: Update physics object "single"

--- tests/physics_objects/test_single.py
+++ tests/physics_objects/test_single.py
@@ -6,11 +6,10 @@ from hml.physics_objects.single import Single, is_single
 def test_init():
     obj = Single(branch="jet", index=0)
 
+    assert obj.branch == "jet"
     assert obj.index == 0
 
     assert obj.name == "jet0"
-    assert obj.branch == "jet"
-    assert obj.slices == [slice(0, 1)]
     assert obj.config == {"branch": "jet", "index": 0}
 
 
@@ -18,7 +17,6 @@ def test_special_methods():
     obj = Single(branch="jet", index=0)
 
     assert obj == Single(branch="jet", index=0)
-    assert obj == "jet0"
     assert str(obj) == "jet0"
     assert repr(obj) == "Single(branch='jet', index=0)"
 
