fcb3a69986526277d151fe0144e7edebc9149274
Star9daisy <star9daisy@outlook.com>
2024-03-07 08:24:05 +0000

    test: Add tests for observable "kinematics"

--- tests/observables/test_kinematics.py
+++ tests/observables/test_kinematics.py
@@ -8,7 +8,7 @@ from hml.observables import kinematics
 def test_init():
     obs = kinematics.Pt(physics_object="jet0")
 
-    assert obs.physics_object == "jet0"
+    assert obs.physics_object.name == "jet0"
     assert obs.class_name == "Pt"
 
     assert obs.name == "jet0.Pt"
@@ -18,7 +18,14 @@ def test_init():
     # Other initializations
     assert kinematics.Pt(physics_object=physics_objects.parse("jet0")).name == "jet0.Pt"
     assert kinematics.Pt(physics_object="jet0", class_name="pt").name == "jet0.pt"
-    assert kinematics.Pt().name == "Pt"
+
+    assert kinematics.Px(physics_object="jet0").name == "jet0.Px"
+    assert kinematics.Py(physics_object="jet0").name == "jet0.Py"
+    assert kinematics.Pz(physics_object="jet0").name == "jet0.Pz"
+    assert kinematics.E(physics_object="jet0").name == "jet0.E"
+    assert kinematics.Eta(physics_object="jet0").name == "jet0.Eta"
+    assert kinematics.Phi(physics_object="jet0").name == "jet0.Phi"
+    assert kinematics.M(physics_object="jet0").name == "jet0.M"
 
 
 def test_special_methods():
@@ -27,7 +34,7 @@ def test_special_methods():
     assert obs == kinematics.Pt(physics_object="jet0")
     assert obs == "jet0.pt"
     assert obs == "Jet0.transverse_momentum"
-    assert str(obs) == "jet0.Pt"
+    assert str(obs) == "jet0.Pt: 0 * unknown"
     assert repr(obs) == "jet0.Pt: 0 * unknown"
 
 
@@ -40,9 +47,6 @@ def test_class_methods():
     with pytest.raises(ValueError):
         kinematics.Pt.from_name("jet0,jet1")
 
-    with pytest.raises(ValueError):
-        kinematics.Pt.from_name("jet0.px")
-
 
 def test_read(events):
     cut = events["Jet_size"].array() > 0
@@ -54,11 +58,7 @@ def test_read(events):
     assert str(obs.value.type) == f"{ak.sum(cut)} * var * var * float32"
 
     obs = kinematics.Pt(physics_object="jet:10.constituents:100").read(events)
-    assert str(obs.value.type) == f"{ak.sum(cut)} * 10 * 100 * float32"
+    assert str(obs.value.type) == f"{ak.sum(cut)} * 10 * 100 * ?float32"
 
     obs = kinematics.Px(physics_object="jet0").read(events)
     assert str(obs.value.type) == f"{ak.sum(cut)} * 1 * float32"
-
-    obs = kinematics.Pt()
-    with pytest.raises(ValueError):
-        obs.read(events)
