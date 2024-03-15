d28ae4493c85c4f8e42f37ea8f440f89323c32e9
Star9daisy <star9daisy@outlook.com>
2024-03-15 06:22:37 +0000

    refactor: Hide approaches, generators, metrics at the top level

--- hml/__init__.py
+++ hml/__init__.py
@@ -1,10 +1,13 @@
 import fastjet
 
-from hml import approaches
-from hml import datasets
-from hml import generators
-from hml import metrics
-from hml import observables
-from hml import representations
+from hml import (
+    # approaches,
+    datasets,
+    # generators,
+    # metrics,
+    observables,
+    physics_objects,
+    representations,
+)
 
 __version__ = "0.4.0"
