c4901459c36994560fb6baa0168475514bf15802
Star9daisy <star9daisy@outlook.com>
2024-03-25 13:55:28 +0000

    feat: Add a new module "operations"

---
+++ hml/operations/__init__.py
@@ -0,0 +1,7 @@
+from .keras_ops import ops_histogram_fixed_width, ops_unique
+from .uproot_ops import (
+    branch_to_momentum4d,
+    constituents_to_momentum4d,
+    find_eflow_in_refs,
+    take_momentum4d,
+)
---
+++ hml/operations/keras_ops.py
@@ -0,0 +1,42 @@
+from keras import ops
+
+
+def ops_histogram_fixed_width(values, value_range, nbins, dtype="int32"):
+    value_min, value_max = value_range
+    bin_edges = ops.linspace(value_min, value_max, nbins + 1)
+    lower = bin_edges[:-1]  # type: ignore
+    upper = bin_edges[1:]  # type: ignore
+
+    return ops.fori_loop(
+        0,
+        nbins,
+        lambda i, s: ops.scatter_update(
+            s,
+            [[i]],
+            [
+                ops.count_nonzero(
+                    ops.where(
+                        ops.logical_and(lower[i] <= values, values <= upper[i]),
+                        1.0,
+                        0.0,
+                    )
+                )
+            ],
+        ),
+        ops.zeros((nbins,), dtype=dtype),
+    )
+
+
+def ops_unique(tensor):
+    sorted_tensor, sorted_indices = ops.sort(tensor), ops.argsort(tensor)
+    selection = ops.not_equal(sorted_tensor[1:], sorted_tensor[:-1])  # type: ignore
+    selection = ops.add(ops.squeeze(ops.where(selection), 0), 1)
+    unique_indices = ops.append([0], selection)
+
+    try:
+        unique_elements = ops.take(sorted_tensor, unique_indices)
+        unique_indices = ops.take(sorted_indices, unique_indices)
+        return unique_elements
+
+    except Exception:
+        return ops.array([0])
