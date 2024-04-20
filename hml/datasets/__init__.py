3b08db03769031a6480f673fa19b9e082a987943
Star9daisy <star9daisy@outlook.com>
2024-04-20 13:02:48 +0000

    feat: Add load_dataset and load_approach functions

--- hml/datasets/__init__.py
+++ hml/datasets/__init__.py
@@ -1,3 +1,20 @@
+import json
+import zipfile
+
 from .graph_dataset import GraphDataset
 from .image_dataset import ImageDataset
 from .set_dataset import SetDataset
+
+
+def load_dataset(filepath, lazy=True):
+    zf = zipfile.ZipFile(filepath)
+    # Extract and read configs JSON
+    with zf.open("configs.json") as json_file:
+        configs = json.load(json_file)
+
+    if configs["class_name"] == "SetDataset":
+        ds = SetDataset.load(filepath, lazy=lazy)
+    else:
+        ds = ImageDataset.load(filepath, lazy=lazy)
+
+    return ds
