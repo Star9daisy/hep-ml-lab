c1b102d397ada657f51baa41356146d5cce4d984
Star9daisy <star9daisy@outlook.com>
2024-04-29 07:46:50 +0000

    feat: Max significance now accepts cross sections, luminosity and weights

--- hml/metrics/max_significance.py
+++ hml/metrics/max_significance.py
@@ -1,21 +1,42 @@
 from __future__ import annotations
 
 from keras import ops
-from keras.metrics import FalsePositives, Metric, TruePositives
+from keras.metrics import (
+    FalseNegatives,
+    FalsePositives,
+    Metric,
+    TrueNegatives,
+    TruePositives,
+)
 
 
 class MaxSignificance(Metric):
     def __init__(
         self,
+        cross_sections=[1, 1],
+        luminosity=1,
+        weights=[1, 1],
         thresholds=None,
         class_id=1,
         name="max_significance",
         dtype=None,
     ):
         super().__init__(name=name, dtype=dtype)
+        self.luminosity = luminosity
+
+        self.cross_sections = cross_sections
+        self.s_xsec = cross_sections.pop(class_id)
+        self.b_xsec = cross_sections
+
+        self.weights = weights
+        self.s_weight = weights.pop(class_id)
+        self.b_weight = weights
+
         self.class_id = class_id
-        self.tp = TruePositives(thresholds=thresholds, dtype=dtype)
-        self.fp = FalsePositives(thresholds=thresholds, dtype=dtype)
+        self.metric_tp = TruePositives(thresholds=thresholds, dtype=dtype)
+        self.metric_fp = FalsePositives(thresholds=thresholds, dtype=dtype)
+        self.metric_tn = TrueNegatives(thresholds=thresholds, dtype=dtype)
+        self.metric_fn = FalseNegatives(thresholds=thresholds, dtype=dtype)
 
     def update_state(self, y_true, y_pred, sample_weight=None):
         y_t_ndim = ops.ndim(ops.squeeze(y_true))
@@ -24,12 +45,32 @@ class MaxSignificance(Metric):
         y_true = ops.argmax(y_true, -1) if y_t_ndim == 2 else y_true
         y_pred = ops.take(y_pred, self.class_id, -1) if y_p_ndim == 2 else y_pred
 
-        self.tp.update_state(y_true, y_pred, sample_weight)
-        self.fp.update_state(y_true, y_pred, sample_weight)
+        self.metric_tp.update_state(y_true, y_pred)
+        self.metric_fp.update_state(y_true, y_pred)
+        self.metric_tn.update_state(y_true, y_pred)
+        self.metric_fn.update_state(y_true, y_pred)
 
     def result(self):
-        s = self.tp.result()
-        b = self.fp.result()
+        if self.cross_sections == [1, 1]:
+            s = self.metric_tp.result()
+            b = self.metric_fp.result()
+
+        else:
+            tpr = self.metric_tp.result() / (
+                self.metric_tp.result() + self.metric_fn.result()
+            )
+            fpr = self.metric_fp.result() / (
+                self.metric_fp.result() + self.metric_tn.result()
+            )
+
+            s = self.s_xsec * self.luminosity * self.s_weight * tpr
+            b = sum(
+                [
+                    xsec * self.luminosity * weight * fpr
+                    for xsec, weight in zip(self.b_xsec, self.b_weight)
+                ]
+            )
+
         significance = ops.divide(s, ops.sqrt(s + b))
         max_significance = ops.max(significance)
         return max_significance
