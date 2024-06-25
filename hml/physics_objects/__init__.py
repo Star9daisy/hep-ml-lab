2d9059962f764b86f7be33c83d2a0878f409800f
Star9daisy <star9daisy@outlook.com>
2024-06-25 08:47:05 +0000

    feat: Update the "parse_physics_object" using the new registration

--- hml/physics_objects/__init__.py
+++ hml/physics_objects/__init__.py
@@ -1,3 +1,10 @@
+from ..registration import (
+    deserialize,
+    is_registered,
+    retrieve,
+    search_registered_classes,
+)
+from ..types import PhysicsObject
 from .electron import Electron
 from .jet import Jet
 from .missing_et import MET, MissingET
@@ -6,3 +13,18 @@ from .photon import Photon
 from .physics_object import PhysicsObjectBase
 from .tower import Tower
 from .track import Track
+
+
+def parse_physics_object(name: str) -> PhysicsObject:
+    """Parse a physics object from its name."""
+    if is_registered(name):
+        obj = retrieve(name)
+
+    else:
+        item = search_registered_classes(name)
+        obj = deserialize(item["serialized_obj"])
+
+    if isinstance(obj, type):
+        return obj.from_name(name)
+    else:
+        return obj
