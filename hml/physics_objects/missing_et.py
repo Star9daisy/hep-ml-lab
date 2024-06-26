fa83c4d1637b6896eeff7e69d77deb23f3ab897a
Star9daisy <star9daisy@outlook.com>
2024-06-26 09:47:12 +0000

    fix: Missing name cases for MET

--- hml/physics_objects/missing_et.py
+++ hml/physics_objects/missing_et.py
@@ -1,6 +1,8 @@
 import re
 from typing import Self
 
+import inflection
+
 from ..naming import str_to_index
 from ..registration import register
 from .physics_object import PhysicsObjectBase
@@ -9,7 +11,10 @@ from .physics_object import PhysicsObjectBase
 class MissingET(PhysicsObjectBase):
     @classmethod
     def from_name(cls, name: str) -> Self:
-        key_pattern = rf"({cls.__name__}|{cls.__name__.lower()}|MET|met)"
+        camel_case = cls.__name__
+        snake_case = inflection.underscore(camel_case)
+        lower_case = cls.__name__.lower()
+        key_pattern = rf"({camel_case}|{lower_case}|{snake_case}|MET|met)"
         indices_pattern = r"(\d*:?\d*)"
         match = re.fullmatch(rf"{key_pattern}{indices_pattern}", name)
         key, indices = match.groups()
@@ -22,6 +27,7 @@ MET = MissingET
 
 
 register(MissingET, "MissingET", existing_ok=True)
+register(MissingET, "missinget", existing_ok=True)
 register(MissingET, "missing_et", existing_ok=True)
 register(MissingET, "MET", existing_ok=True)
 register(MissingET, "met", existing_ok=True)
