10fde683f58f79a17565cda04e0e03fa712cd7d1
Star9daisy <star9daisy@outlook.com>
2024-08-28 08:17:05 +0000

    tests: Add missing tests for "config" module

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,4 +1,9 @@
-from hml.config import get_custom_objects_file_path, set_custom_objects_file_path
+from hml.config import (
+    get_custom_objects_file_path,
+    get_registry_file_path,
+    set_custom_objects_file_path,
+    set_registry_file_path,
+)
 
 
 def test_get_custom_objects_file_path():
@@ -8,4 +13,12 @@ def test_get_custom_objects_file_path():
 def test_set_custom_objects_file_path():
     set_custom_objects_file_path("tests/saving/custom_objects.py")
     assert get_custom_objects_file_path().as_posix() == "tests/saving/custom_objects.py"
-    assert get_custom_objects_file_path().exists()
+
+
+def test_get_registry_file_path():
+    assert get_registry_file_path().as_posix() == "registry.json"
+
+
+def test_set_registry_file_path():
+    set_registry_file_path("tests/saving/registry.json")
+    assert get_registry_file_path().as_posix() == "tests/saving/registry.json"
