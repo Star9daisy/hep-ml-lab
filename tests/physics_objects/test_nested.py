b82b42552b020cbdc340e58c995437074bcd9f01
Star9daisy <star9daisy@outlook.com>
2024-09-10 13:46:56 +0000

    tests: Update tests for single and nested physics objects

--- tests/physics_objects/test_nested.py
+++ tests/physics_objects/test_nested.py
@@ -1,57 +1,135 @@
 import pytest
 
 from hml.events import load_events
-from hml.physics_objects import Constituents, Jet
+from hml.physics_objects import Constituents, Electron, Jet, Reclustered
 from hml.saving import retrieve
 
 
-def test_constituents(root_events_path):
-    events = load_events(root_events_path)
+@pytest.fixture
+def events(root_events_path):
+    return load_events(root_events_path)
 
-    # Check __init__
-    csts = Constituents("jet")
-    # Or
-    csts = Constituents(Jet())
 
-    # Check __repr__
-    assert repr(csts) == "jet.constituents -> not read yet"
+def test_constituents(events):
+    obj = Constituents(Jet())
+    # Bad case
+    with pytest.raises(ValueError):
+        Constituents(Electron())
 
-    # Check properties
-    assert csts.key == "constituents"
-    assert csts.index == slice(None)
-    assert csts.array.typestr == "0 * unknown"
+    # Special methods -------------------------------------------------------- #
+    assert repr(obj) == "jet.constituents: not read yet"
 
-    # Check read
-    assert csts.read(events)  # Ensure the returned value is not None
-
-    assert repr(csts) == "jet.constituents -> 100 * var * var * {4 observables}"
-    assert csts.array.typestr[:19] == "100 * var * var * {"
-    assert csts.array.fields == ["pt", "eta", "phi", "mass"]
-
-    # Check config
-    assert csts.config == {
+    # Properties ------------------------------------------------------------- #
+    assert obj.source.name == "jet"
+    assert obj.key == "constituents"
+    assert obj.index == slice(None)
+    assert obj.array.typestr == "0 * unknown"
+    assert obj.name == "jet.constituents"
+    assert obj.config == {
         "source": "jet",
         "key": "constituents",
         "index": "",
         "name": "jet.constituents",
     }
 
-    # Check from_config
-    assert Constituents.from_config(csts.config).config == csts.config
+    # Methods ---------------------------------------------------------------- #
+    assert obj.read(events)
+    assert repr(obj) == "jet.constituents: 4 observables"
+    assert obj.array.typestr.startswith("100 * var * var * {")
+    assert obj.array.fields == ["pt", "eta", "phi", "mass"]
 
-    # Check array typestr
-    csts = Constituents("jet").read(events)
-    assert csts.array.typestr[:19] == "100 * var * var * {"
-
-    csts = Constituents("jet:10", index=slice(100)).read(events)
-    assert csts.array.typestr[:19] == "100 * 10 * 100 * ?{"
-
-    csts = Constituents("jet0", index=slice(10)).read(events)
-    assert csts.array.typestr[:12] == "100 * 10 * ?"
-
-    # Check registered names
-    assert retrieve("jet0.constituents")
-
+    # Class methods ---------------------------------------------------------- #
+    assert Constituents.from_config(obj.config).config == obj.config
     # Bad case
     with pytest.raises(ValueError):
-        Constituents("unknown")
+        Constituents.from_config({"source": "unknown"})
+
+    # Others ----------------------------------------------------------------- #
+    # Check array shape for different indices
+    source = Reclustered(Jet(algorithm="ak", radius=1.0))
+    obj = Constituents(source).read(events)
+    assert obj.array.typestr.startswith("100 * var * var * var * {")
+
+    source = Reclustered(Jet(algorithm="ak", radius=1.0, index=0))
+    obj = Constituents(source).read(events)
+    assert obj.array.typestr.startswith("100 * var * var * {")
+
+    source = Reclustered(Jet(algorithm="ak", radius=1.0, index=0), index=0)
+    obj = Constituents(source).read(events)
+    assert obj.array.typestr.startswith("100 * var * {")
+
+    source = Reclustered(Jet(algorithm="ak", radius=1.0, index=0), index=0)
+    obj = Constituents(source, index=0).read(events)
+    assert obj.array.typestr.startswith("100 * {")
+
+    # Check registered names
+    assert retrieve("jet.constituents")
+    assert retrieve("ak10jet.constituents")
+    assert retrieve("ak10jet.reclustered.constituents")
+
+
+def test_reclustered(events):
+    obj = Reclustered(Jet(algorithm="ak", radius=1.0))
+    # Bad case
+    with pytest.raises(ValueError):
+        Reclustered(Electron())
+    with pytest.raises(ValueError):
+        Reclustered(Jet())
+
+    # Special methods -------------------------------------------------------- #
+    assert repr(obj) == "ak10jet.reclustered: not read yet"
+
+    # Properties ------------------------------------------------------------- #
+    assert obj.source.name == "ak10jet"
+    assert obj.key == "reclustered"
+    assert obj.index == slice(None)
+    assert obj.array.typestr == "0 * unknown"
+    assert obj.name == "ak10jet.reclustered"
+    assert obj.config == {
+        "source": "ak10jet",
+        "key": "reclustered",
+        "index": "",
+        "name": "ak10jet.reclustered",
+    }
+
+    # Methods ---------------------------------------------------------------- #
+    assert obj.read(events)
+    assert repr(obj) == "ak10jet.reclustered: 4 observables"
+    assert obj.array.typestr.startswith("100 * var * var * {")
+    assert obj.array.fields == ["pt", "eta", "phi", "mass"]
+
+    # Class methods ---------------------------------------------------------- #
+    assert Reclustered.from_config(obj.config).config == obj.config
+    # Bad case
+    with pytest.raises(ValueError):
+        Reclustered.from_config({"source": "unknown"})
+
+    # Others ----------------------------------------------------------------- #
+    # Check array shape for different indices
+    source = Jet(algorithm="ak", radius=1.0)
+
+    obj = Reclustered(source, index=0).read(events)
+    assert obj.array.typestr.startswith("100 * var * {")
+
+    obj = Reclustered(source, index=slice(1)).read(events)
+    assert obj.array.typestr.startswith("100 * var * 1 * {")
+
+    obj = Reclustered(source, index=slice(10)).read(events)
+    assert obj.array.typestr.startswith("100 * var * 10 * ?{")
+
+    obj = Reclustered(source, index=slice(1, None)).read(events)
+    assert obj.array.typestr.startswith("100 * var * var * {")
+
+    obj = Reclustered(source, index=slice(100, None)).read(events)
+    assert obj.array.typestr.startswith("100 * var * var * {")
+
+    source = Jet(algorithm="ak", radius=1.0, index=0)
+    obj = Reclustered(source, index=slice(100, None)).read(events)
+    assert obj.array.typestr.startswith("100 * var * {")
+
+    source = Jet(algorithm="ak", radius=1.0, index=slice(100, None))
+    obj = Reclustered(source, index=slice(100, None)).read(events)
+    assert obj.array.typestr.startswith("100 * var * var * {")
+
+    # Check registered names
+    assert retrieve("ak10jet.reclustered")
--- tests/physics_objects/test_single.py
+++ tests/physics_objects/test_single.py
@@ -1,72 +1,76 @@
+import pytest
+
 from hml.events import load_events
 from hml.physics_objects import Electron, Jet, MissingET, Muon, Photon, Tower, Track
 from hml.saving import retrieve
 
 
-def test_electron(root_events_path):
-    events = load_events(root_events_path)
+@pytest.fixture
+def events(root_events_path):
+    return load_events(root_events_path)
 
-    # Check __init__
-    e = Electron()
 
-    # Check __repr__
-    assert repr(e) == "electron -> not read yet"
+def test_electron(events):
+    obj = Electron()
 
-    # Check properties
-    assert e.key == "electron"
-    assert e.index == slice(None)
-    assert e.array.typestr == "0 * unknown"
+    # Special methods -------------------------------------------------------- #
+    assert repr(obj) == "electron: not read yet"
 
-    # Check read
-    assert e.read(events)  # Ensure the returned value is not None
+    # Properties ------------------------------------------------------------- #
+    assert obj.key == "electron"
+    assert obj.index == slice(None)
+    assert obj.array.typestr == "0 * unknown"
+    assert obj.name == "electron"
+    assert obj.config == {"key": "electron", "index": "", "name": "electron"}
 
-    assert repr(e) == "electron -> 100 * 0 * {5 observables}"
-    assert e.array.fields == ["pt", "eta", "phi", "mass", "charge"]
+    # Methods ---------------------------------------------------------------- #
+    assert obj.read(events)
+    assert repr(obj) == "electron: 5 observables"
+    assert obj.array.typestr.startswith("100 * 0 * {")
+    assert obj.array.fields == ["pt", "eta", "phi", "mass", "charge"]
 
-    # Check config
-    assert e.config == {
-        "key": "electron",
-        "index": "",
-        "name": "electron",
-    }
+    # Class methods ---------------------------------------------------------- #
+    assert Electron.from_config(obj.config).config == obj.config
 
-    # Check from_config
-    assert Electron.from_config(e.config).config == e.config
+    # Others ----------------------------------------------------------------- #
+    # Check array shape for different indices
+    obj = Electron(index=0).read(events)
+    assert obj.array.typestr.startswith("100 * ?{")
 
-    # Check array typestr
-    e = Electron(index=0).read(events)
-    assert e.array.typestr[:7] == "100 * ?"
+    obj = Electron(index=slice(10)).read(events)
+    assert obj.array.typestr.startswith("100 * 10 * ?{")
 
-    e = Electron(index=slice(10)).read(events)
-    assert e.array.typestr[:12] == "100 * 10 * ?"
-
-    e = Electron(index=slice(10, None)).read(events)
-    assert e.array.typestr[:7] == "100 * 0"
+    obj = Electron(index=slice(10, None)).read(events)
+    assert obj.array.typestr.startswith("100 * 0 * {")
 
     # Check registered names
     assert retrieve("electron")
 
 
-def test_jet(root_events_path):
-    events = load_events(root_events_path)
+def test_directly_retrieved_jet(events):
+    obj = Jet()
 
-    # Check __init__
-    jet = Jet()
+    # Special methods -------------------------------------------------------- #
+    assert repr(obj) == "jet: not read yet"
 
-    # Check __repr__
-    assert repr(jet) == "jet -> not read yet"
+    # Properties ------------------------------------------------------------- #
+    assert obj.key == "jet"
+    assert obj.index == slice(None)
+    assert obj.array.typestr == "0 * unknown"
+    assert obj.name == "jet"
+    assert obj.config == {
+        "key": "jet",
+        "index": "",
+        "name": "jet",
+        "algorithm": None,
+        "radius": None,
+    }
 
-    # Check properties
-    assert jet.key == "jet"
-    assert jet.index == slice(None)
-    assert jet.array.typestr == "0 * unknown"
-
-    # Check read
-    assert jet.read(events)  # Ensure the returned value is not None
-
-    assert repr(jet) == "jet -> 100 * var * {12 observables}"
-    assert jet.array.typestr[:9] == "100 * var"
-    assert jet.array.fields == [
+    # Methods ---------------------------------------------------------------- #
+    assert obj.read(events)
+    assert repr(obj) == "jet: 12 observables"
+    assert obj.array.typestr.startswith("100 * var * {")
+    assert obj.array.fields == [
         "pt",
         "eta",
         "phi",
@@ -81,86 +85,128 @@ def test_jet(root_events_path):
         "tau5",
     ]
 
-    # Check config
-    assert jet.config == {
-        "algorithm": None,
-        "radius": None,
-        "key": "jet",
-        "index": "",
-        "name": "jet",
-    }
+    # Class methods ---------------------------------------------------------- #
+    assert Jet.from_config(obj.config).config == obj.config
 
-    # Check from_config
-    assert Jet.from_config(jet.config).config == jet.config
+    # Others ----------------------------------------------------------------- #
+    # Check array shape for different indices
+    obj = Jet(index=0).read(events)
+    assert obj.array.typestr.startswith("100 * {")
 
-    # Check array typestr
-    jet = Jet(index=0).read(events)
-    assert jet.array.typestr[:7] == "100 * {"
+    obj = Jet(index=10).read(events)
+    assert obj.array.typestr.startswith("100 * ?{")
 
-    jet = Jet(index=10).read(events)
-    assert jet.array.typestr[:7] == "100 * ?"
+    obj = Jet(index=slice(1)).read(events)
+    assert obj.array.typestr.startswith("100 * 1 * {")
 
-    jet = Jet(index=slice(1)).read(events)
-    assert jet.array.typestr[:11] == "100 * 1 * {"
+    obj = Jet(index=slice(10)).read(events)
+    assert obj.array.typestr.startswith("100 * 10 * ?")
 
-    jet = Jet(index=slice(10)).read(events)
-    assert jet.array.typestr[:12] == "100 * 10 * ?"
+    obj = Jet(index=slice(1, None)).read(events)
+    assert obj.array.typestr.startswith("100 * var * {")
 
-    jet = Jet(index=slice(1, None)).read(events)
-    assert jet.array.typestr[:13] == "100 * var * {"
+    obj = Jet(index=slice(10, None)).read(events)
+    assert obj.array.typestr.startswith("100 * 0 * {")
 
-    jet = Jet(index=slice(10, None)).read(events)
-    assert jet.array.typestr[:11] == "100 * 0 * {"
-
-    jet = Jet(index=slice(0, 10)).read(events)
-    assert jet.array.typestr[:12] == "100 * 10 * ?"
+    obj = Jet(index=slice(0, 10)).read(events)
+    assert obj.array.typestr.startswith("100 * 10 * ?")
 
     # Check registered names
     assert retrieve("jet")
+    assert retrieve("fatjet")
     assert retrieve("fat_jet")
 
 
-def test_missing_et(root_events_path):
-    events = load_events(root_events_path)
+def test_clustered_jet(events):
+    obj = Jet(algorithm="kt", radius=0.4)
 
-    missing_et = MissingET().read(events)
+    # Special methods -------------------------------------------------------- #
+    assert repr(obj) == "kt4jet: not read yet"
 
-    assert missing_et.array.fields == ["pt", "eta", "phi", "mass"]
+    # Properties ------------------------------------------------------------- #
+    assert obj.key == "jet"
+    assert obj.index == slice(None)
+    assert obj.array.typestr == "0 * unknown"
+    assert obj.name == "kt4jet"
+    assert obj.config == {
+        "key": "jet",
+        "index": "",
+        "name": "kt4jet",
+        "algorithm": "kt",
+        "radius": 0.4,
+    }
+
+    # Methods ---------------------------------------------------------------- #
+    assert obj.read(events)
+    assert repr(obj) == "kt4jet: 4 observables"
+    assert obj.array.fields == ["pt", "eta", "phi", "mass"]
+
+    # Class methods ---------------------------------------------------------- #
+    assert Jet.from_config(obj.config).config == obj.config
+
+    # Others ----------------------------------------------------------------- #
+    # Check registered names
+    assert retrieve("kt4jet")
+    assert retrieve("ca8fat_jet")
+    assert retrieve("ak10fatjet")
+
+
+def test_missing_et(events):
+    obj = MissingET()
+
+    # Methods ---------------------------------------------------------------- #
+    assert obj.read(events)
+    assert obj.array.fields == ["pt", "eta", "phi", "mass"]
+
+    # Others ----------------------------------------------------------------- #
+    # Check registered names
     assert retrieve("missing_et")
     assert retrieve("met")
 
 
-def test_muon(root_events_path):
-    events = load_events(root_events_path)
+def test_muon(events):
+    obj = Muon()
 
-    muon = Muon().read(events)
+    # Methods ---------------------------------------------------------------- #
+    assert obj.read(events)
+    assert obj.array.fields == ["pt", "eta", "phi", "mass", "charge"]
 
-    assert muon.array.fields == ["pt", "eta", "phi", "mass", "charge"]
+    # Others ----------------------------------------------------------------- #
+    # Check registered names
     assert retrieve("muon")
 
 
-def test_photon(root_events_path):
-    events = load_events(root_events_path)
+def test_photon(events):
+    obj = Photon()
 
-    photon = Photon().read(events)
+    # Methods ---------------------------------------------------------------- #
+    assert obj.read(events)
+    assert obj.array.fields == ["pt", "eta", "phi", "mass"]
 
-    assert photon.array.fields == ["pt", "eta", "phi", "mass"]
+    # Others ----------------------------------------------------------------- #
+    # Check registered names
     assert retrieve("photon")
 
 
-def test_tower(root_events_path):
-    events = load_events(root_events_path)
+def test_tower(events):
+    obj = Tower()
 
-    tower = Tower().read(events)
+    # Methods ---------------------------------------------------------------- #
+    assert obj.read(events)
+    assert obj.array.fields == ["pt", "eta", "phi", "mass"]
 
-    assert tower.array.fields == ["pt", "eta", "phi", "mass"]
+    # Others ----------------------------------------------------------------- #
+    # Check registered names
     assert retrieve("tower")
 
 
-def test_track(root_events_path):
-    events = load_events(root_events_path)
+def test_track(events):
+    obj = Track()
 
-    track = Track().read(events)
+    # Methods ---------------------------------------------------------------- #
+    assert obj.read(events)
+    assert obj.array.fields == ["pt", "eta", "phi", "mass"]
 
-    assert track.array.fields == ["pt", "eta", "phi", "mass"]
+    # Others ----------------------------------------------------------------- #
+    # Check registered names
     assert retrieve("track")
