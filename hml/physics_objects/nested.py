395936613b38c34b99a0ccca96939fc1e330ddb8
Star9daisy <star9daisy@outlook.com>
2024-09-10 13:46:43 +0000

    fix & feat: Fix the wrong type of source and constituents; add constituents as an attribute

--- hml/physics_objects/nested.py
+++ hml/physics_objects/nested.py
@@ -10,21 +10,16 @@ from ..naming import INDEX_PATTERN
 from ..operations.awkward_ops import pad_none
 from ..operations.fastjet_ops import get_algorithm
 from ..saving import registered_object, retrieve
-from ..types import AwkwardArray, index_to_str, str_to_index
+from ..types import AwkwardArray, array_to_momentum, index_to_str, str_to_index
 from .physics_object import PhysicsObject
+from .single import Jet
 
 
 @typechecked
 class Nested(PhysicsObject):
-    def __init__(self, source: str | PhysicsObject, **kwargs) -> None:
+    def __init__(self, source: PhysicsObject, **kwargs) -> None:
         super().__init__(**kwargs)
-
-        if isinstance(source, str):
-            self._source = retrieve(source)
-            if self._source is None:
-                raise ValueError(f"Physics object {self._source} not found")
-        else:
-            self._source = source
+        self._source = source
 
         if kwargs.get("name") is None:
             self._name = self.source.name + "." + self._name
@@ -38,7 +33,12 @@ class Nested(PhysicsObject):
 
     def read(self, events):
         array = self.get_array(events)
-        indices = [self.source.index, self.index]
+
+        if isinstance(self.source, Nested):
+            indices = [self.source.source.index, self.source.index, self.index]
+        else:
+            indices = [self.source.index, self.index]
+
         for i, index in enumerate(indices, start=1):
             if isinstance(index, int):
                 target = index + 1
@@ -51,12 +51,6 @@ class Nested(PhysicsObject):
                 array = pad_none(array, target, i)
 
         self._array = array[tuple([slice(None), *indices])]
-
-        try:
-            self._array = ak.to_regular(self._array, axis=None)
-        except Exception:
-            pass
-
         return self
 
     @property
@@ -70,8 +64,13 @@ class Nested(PhysicsObject):
 
     @classmethod
     def from_config(cls, config: dict) -> Self:
+        source = retrieve(config["source"])
+
+        if source is None:
+            raise ValueError(f"Source {config['source']} not found")
+
         return cls(
-            source=config["source"],
+            source=source,
             key=config["key"],
             index=str_to_index(config["index"]),
             name=config.get("name"),
@@ -81,9 +80,15 @@ class Nested(PhysicsObject):
 @typechecked
 @registered_object(rf"(?P<source>[\w:._]+)\.(?P<key>constituents){INDEX_PATTERN}")
 class Constituents(Nested):
+    def __init__(self, source: PhysicsObject, **kwargs) -> None:
+        super().__init__(source, **kwargs)
+
+        if not isinstance(self.source, (Jet, Reclustered)):
+            raise ValueError("Source currently must be a jet or a reclustered jet")
+
     def get_array(self, events: ROOTEvents) -> AwkwardArray:
-        self.source.read(events)
-        if hasattr(self.source, "constituents"):
+        if isinstance(self.source, Reclustered):
+            self.source.read(events)
             return self.source.constituents
 
         array = events[self.source.key + "." + self.key]
@@ -102,20 +107,37 @@ class Constituents(Nested):
 @typechecked
 @registered_object(rf"(?P<source>[\w:._]+)\.(?P<key>reclustered){INDEX_PATTERN}")
 class Reclustered(Nested):
+    def __init__(self, source: PhysicsObject, **kwargs) -> None:
+        super().__init__(source, **kwargs)
+
+        if not isinstance(self.source, Jet):
+            raise ValueError("Source must be a jet")
+
+        if self.source.algorithm is None or self.source.radius is None:
+            raise ValueError("Source must have algorithm and radius")
+
     def get_array(self, events: ROOTEvents) -> AwkwardArray:
         self.source.read(events)
+        constituents = array_to_momentum(self.source.constituents)
+        algorithm = self.source.algorithm
+        radius = self.source.radius
 
-        self.constituents = self.source.constituents
-        self.algorithm = self.source.algorithm
-        self.radius = self.source.radius
-
-        jet_def = fj.JetDefinition(get_algorithm(self.algorithm), self.radius)
-        cluster = fj.ClusterSequence(self.constituents, jet_def)
+        jet_def = fj.JetDefinition(get_algorithm(algorithm), radius)
+        cluster = fj.ClusterSequence(constituents, jet_def)
         jets = cluster.inclusive_jets()
+
         sort_indices = ak.argsort(jets.pt, ascending=False)
         jets = ak.zip(
             {"pt": jets.pt, "eta": jets.eta, "phi": jets.phi, "mass": jets.mass}
         )
         jets = jets[sort_indices]
+        self.constituents = ak.zip(
+            {
+                "pt": cluster.constituents().pt,
+                "eta": cluster.constituents().eta,
+                "phi": cluster.constituents().phi,
+                "mass": cluster.constituents().mass,
+            }
+        )
 
         return ak.values_astype(jets, "float32")
