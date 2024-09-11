55c44463396ae906a47a71c548d0d9f8d72ac93a
Star9daisy <star9daisy@outlook.com>
2024-09-11 06:37:09 +0000

    feat: Expose InvM and parse_observable to module level

--- hml/observables/__init__.py
+++ hml/observables/__init__.py
@@ -1,13 +1,13 @@
 # isort: off
 from .observable import Observable
-
-# from .utils import parse_observable
+from .utils import parse_observable
 
 # isort: on
 from .angular_distance import DeltaR
 from .azimuthal_angle import Phi
 from .charge import Charge
 from .energy import E
+from .invariant_mass import InvM
 from .mass import M
 from .momentum_x import Px
 from .momentum_y import Py
