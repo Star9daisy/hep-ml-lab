e9197bd89269c633b6d0ba8c56dcd2f7ff34f2f3
Star9daisy <star9daisy@outlook.com>
2024-09-11 06:35:55 +0000

    refactor: Update DeltaR and InvM

--- hml/observables/angular_distance.py
+++ hml/observables/angular_distance.py
@@ -1,5 +1,3 @@
-from typing import Self
-
 import awkward as ak
 import numpy as np
 from typeguard import typechecked
@@ -8,64 +6,62 @@ from ..events import Events
 from ..physics_objects import PhysicsObject
 from ..saving import registered_object
 from ..types import AwkwardArray
-from .azimuthal_angle import Phi
 from .observable import Observable
-from .pseudo_rapidity import Eta
 
 
 @typechecked
-@registered_object(r"(?P<physics_object>[\w:._,]+)\.(DeltaR|delta_r)")
+@registered_object(r"(?P<physics_object>[\w:._]+(?:,[\w:._]+)+)\.(DeltaR|delta_r)")
 class DeltaR(Observable):
-    def __init__(self, physics_object: list[str | PhysicsObject], **kwargs) -> None:
+    def __init__(self, physics_object: list[PhysicsObject], **kwargs) -> None:
         super().__init__(physics_object, **kwargs)
+        name = kwargs.get("name")
+        physics_object_names = ",".join([obj.name for obj in self.physics_object])
+        self._name = name if name is not None else f"{physics_object_names}.delta_r"
 
         if len(physics_object) != 2:
             raise ValueError("DeltaR requires exactly two physics objects.")
 
     def get_array(self, events: Events) -> AwkwardArray:
-        eta0 = Eta(self.physics_object[0]).read(events).array
-        phi0 = Phi(self.physics_object[0]).read(events).array
+        self.physics_object[0].read(events)
+        self.physics_object[1].read(events)
+        eta1 = self.physics_object[0].array.eta
+        phi1 = self.physics_object[0].array.phi
+        eta2 = self.physics_object[1].array.eta
+        phi2 = self.physics_object[1].array.phi
 
-        eta1 = Eta(self.physics_object[1]).read(events).array
-        phi1 = Phi(self.physics_object[1]).read(events).array
+        if eta1.ndim > 2:
+            for _ in range(eta1.ndim - 2):
+                eta1 = ak.flatten(eta1, axis=-1)
+                phi1 = ak.flatten(phi1, axis=-1)
+                try:
+                    eta1 = ak.to_regular(eta1, axis=-1)
+                    phi1 = ak.to_regular(phi1, axis=-1)
+                except Exception:  # pragma: no cover
+                    pass
 
-        if eta0.ndim == 3:
-            eta0 = ak.flatten(eta0, axis=-1)
-            phi0 = ak.flatten(phi0, axis=-1)
+        if eta2.ndim > 2:
+            for _ in range(eta2.ndim - 2):
+                eta2 = ak.flatten(eta2, axis=-1)
+                phi2 = ak.flatten(phi2, axis=-1)
+                try:
+                    eta2 = ak.to_regular(eta2, axis=-1)
+                    phi2 = ak.to_regular(phi2, axis=-1)
+                except Exception:  # pragma: no cover
+                    pass
 
-        if eta1.ndim == 3:
-            eta1 = ak.flatten(eta1, axis=-1)
-            phi1 = ak.flatten(phi1, axis=-1)
-
-        if eta0.ndim == 1:
-            eta0 = eta0[:, None]
-            phi0 = phi0[:, None]
+        if eta1.ndim == 2 and eta2.ndim == 2:
+            eta1 = eta1[:, :, None]
+            phi1 = phi1[:, :, None]
+            eta2 = eta2[:, None, :]
+            phi2 = phi2[:, None, :]
 
         if eta1.ndim == 1:
-            eta1 = eta1[:, None]
-            phi1 = phi1[:, None]
+            eta1 = eta1[:, *([None] * (eta2.ndim - 1))]
+            phi1 = phi1[:, *([None] * (eta2.ndim - 1))]
 
-        delta_eta = eta0[:, :, None] - eta1[:, None, :]
-        delta_phi = phi0[:, :, None] - phi1[:, None, :]
-        array = np.hypot(delta_eta, delta_phi)
+        if eta2.ndim == 1:
+            eta2 = eta2[:, *([None] * (eta1.ndim - 1))]
+            phi2 = phi2[:, *([None] * (eta1.ndim - 1))]
+
+        array = np.hypot(eta1 - eta2, phi1 - phi2)
         return array
-
-    @property
-    def name(self) -> str:
-        if self._name:
-            return self._name
-
-        physics_object = ",".join([obj.name for obj in self.physics_object])
-        return f"{physics_object}.delta_r"
-
-    @property
-    def config(self) -> dict:
-        config = super().config
-        config["physics_object"] = [obj.name for obj in self.physics_object]
-        return config
-
-    @classmethod
-    def from_config(cls, config: dict) -> Self:
-        physics_object = config["physics_object"].split(",")
-        name = config.get("name")
-        return cls(physics_object=physics_object, name=name)
--- hml/observables/invariant_mass.py
+++ hml/observables/invariant_mass.py
@@ -1,30 +1,31 @@
 from functools import reduce
 
+import numpy as np
 from typeguard import typechecked
 
 from ..events import Events
 from ..physics_objects import PhysicsObject
-from ..saving import register
+from ..saving import registered_object
 from ..types import AwkwardArray, array_to_momentum
 from .observable import Observable
 
 
-# ---------------------------------------------------------------------------- #
 @typechecked
+@registered_object(r"(?P<physics_object>[\w:._]+)\.(InvariantMass|invariant_mass)")
+@registered_object(r"(?P<physics_object>[\w:._]+)\.(InvMass|inv_mass|InvM|inv_m)")
 class InvM(Observable):
     def __init__(self, physics_object: list[PhysicsObject], **kwargs) -> None:
         super().__init__(physics_object, **kwargs)
 
     def get_array(self, events: Events) -> AwkwardArray:
-        physics_objects = [obj.read(events) for obj in self.physics_object]
-        arrays = [array_to_momentum(obj.array) for obj in physics_objects]
-        summed = reduce(lambda x, y: x + y, arrays)
+        objs = [obj.read(events) for obj in self.physics_object]
+
+        if np.unique([obj.array.ndim for obj in objs]).size > 1:
+            raise ValueError("ndim mismatch")
+
+        if np.unique([obj.array.typestr.split(" * ")[1:-1] for obj in objs]).size > 1:
+            raise ValueError("type mismatch")
+
+        momenta = [array_to_momentum(obj.array) for obj in objs]
+        summed = reduce(lambda x, y: x + y, momenta)
         return summed.mass
-
-
-register(InvM, r"InvariantMass", exists_ok=True)
-register(InvM, r"invariant_mass", exists_ok=True)
-register(InvM, r"InvMass", exists_ok=True)
-register(InvM, r"inv_mass", exists_ok=True)
-register(InvM, r"InvM", exists_ok=True)
-register(InvM, r"inv_m", exists_ok=True)
