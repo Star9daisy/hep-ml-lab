237d12557366b40872e45e6d8804cbec7fc54d4e
Star9daisy <star9daisy@outlook.com>
2024-10-03 11:16:12 +0000

    fix: File paths are changed by chance to raise an error

--- tests/test_config.py
+++ tests/test_config.py
@@ -6,19 +6,11 @@ from hml.config import (
 )
 
 
-def test_get_custom_objects_file_path():
-    assert get_custom_objects_file_path().as_posix() == "custom_objects.py"
-
-
-def test_set_custom_objects_file_path(custom_objects_in_tests):
+def test_get_and_set_custom_objects_file_path(custom_objects_in_tests):
     set_custom_objects_file_path(custom_objects_in_tests)
     assert get_custom_objects_file_path().as_posix() == "tests/saving/custom_objects.py"
 
 
-def test_get_registry_file_path():
-    assert get_registry_file_path().as_posix() == "registry.json"
-
-
-def test_set_registry_file_path(registry_in_tests):
+def test_get_and_set_registry_file_path(registry_in_tests):
     set_registry_file_path(registry_in_tests)
     assert get_registry_file_path().as_posix() == "tests/saving/registry.json"
