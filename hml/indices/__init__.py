7d5989e42ba0170f6778bb6d9e3613fd78fe0613
Star9daisy <star9daisy@gmail.com>
2024-12-12 13:47:26 +0000

    feat: Expose "IndexLike" and "index_like_to_index" to top level of "indices" module

--- hml/indices/__init__.py
+++ hml/indices/__init__.py
@@ -1,4 +1,4 @@
 from .base import Index
 from .integer import IntegerIndex
 from .range import RangeIndex
-from .utils import deserialize, get, serialize
+from .utils import IndexLike, deserialize, get, index_like_to_index, serialize
