4907030aca4b83055963fb63e79d9d2b4969c97e
Star9daisy <star9daisy@gmail.com>
2024-12-14 13:26:54 +0000

    feat: Add missing functions to "NestedPhysicsObject"

--- hml/physics_objects/nested.py
+++ hml/physics_objects/nested.py
@@ -1,8 +1,14 @@
+import re
+from importlib import import_module
+
 import awkward as ak
 from inflection import underscore
 from typeguard import typechecked
 
 from ..indices import Index, IndexLike, IntegerIndex, RangeIndex, index_like_to_index
+from ..indices import deserialize as deserialize_index
+from ..indices import retrieve as retrieve_index
+from ..indices import serialize as serialize_index
 from ..operations import awkward as oak
 from ..operations import fastjet as ofj
 from ..operations import uproot as our
@@ -14,18 +20,21 @@ from ..types import (
     pxpypze_to_ptetaphim,
 )
 from .base import PhysicsObject
-from .single import Jet, SinglePhysicsObject
+from .single import FatJet, Jet, SinglePhysicsObject
 
 
 @typechecked
 class NestedPhysicsObject(PhysicsObject):
+    PATTERN: str = rf"{SinglePhysicsObject.PATTERN}(?:\.{SinglePhysicsObject.PATTERN})+"
+
     def __init__(
         self,
         parent: "SinglePhysicsObject | NestedPhysicsObject",
         index: IndexLike | None = None,
     ) -> None:
         self.parent = parent
-        self._branch = parent.branch + self.__class__.__name__
+        self._branch = self.__class__.__name__
+        self.branch_name = underscore(self._branch)
         self._index = index
         self._array = oak.empty_array()
 
@@ -89,12 +98,37 @@ class NestedPhysicsObject(PhysicsObject):
 
     @property
     def name(self) -> str:
-        class_name = underscore(self.__class__.__name__)
-        return self.parent.name + "." + class_name + self.index.name
+        return f"{self.parent.name}.{self.branch_name + self.index.name}"
+
+    @classmethod
+    def from_name(cls, name: str) -> Self:
+        raise NotImplementedError
+
+    @property
+    def config(self) -> dict:
+        return {
+            "parent": {
+                "module": self.parent.__module__,
+                "class_name": self.parent.__class__.__name__,
+                "config": self.parent.config,
+            },
+            "index": serialize_index(self.index),
+        }
+
+    @classmethod
+    def from_config(cls, config: dict) -> Self:
+        parent_module = import_module(config["parent"]["module"])
+        parent_cls = getattr(parent_module, config["parent"]["class_name"])
+        parent = parent_cls.from_config(config["parent"]["config"])
+        index = deserialize_index(config["index"])
+
+        return cls(parent=parent, index=index)
 
 
 @typechecked
 class Reclustered(NestedPhysicsObject):
+    PATTERN: str = rf"(?P<parent>\w+?\d+(?:fat)?jet{Index.PATTERN})\.reclustered(?P<index>{Index.PATTERN})"
+
     def __init__(self, parent: Jet, index: IndexLike | None = None) -> None:
         super().__init__(parent, index)
 
@@ -123,9 +157,33 @@ class Reclustered(NestedPhysicsObject):
 
         return ak.values_astype(jets, "float32")
 
+    @classmethod
+    def from_name(cls, name: str) -> Self:
+        if not (match := re.fullmatch(cls.PATTERN, name)):
+            raise ValueError(f"Invalid name: {name}")
+
+        group = match.groupdict()
+        branch = group.get("branch")
+
+        if re.fullmatch(Jet.PATTERN, group["parent"]):
+            parent = Jet.from_name(group["parent"])
+        elif re.fullmatch(FatJet.PATTERN, group["parent"]):
+            parent = FatJet.from_name(group["parent"])
+        else:
+            raise ValueError(f"Invalid parent: {group['parent']}")
+
+        index = retrieve_index(group["index"])
+
+        obj = cls(parent=parent, index=index)
+        obj.branch_name = branch or underscore(cls.__name__)
+
+        return obj
+
 
 @typechecked
 class Constituents(NestedPhysicsObject):
+    PATTERN: str = rf"(?P<parent>\w+\d+(?:fat)?jet{Index.PATTERN}(?:\.reclustered{Index.PATTERN})?)\.constituents(?P<index>{Index.PATTERN})"
+
     def __init__(
         self, parent: Jet | Reclustered, index: IndexLike | None = None
     ) -> None:
@@ -136,3 +194,27 @@ class Constituents(NestedPhysicsObject):
 
     def get_array(self, events: ROOTEvents) -> AwkwardArray:
         return self.parent.read(events).constituents
+
+    @classmethod
+    def from_name(cls, name: str) -> Self:
+        if not (match := re.fullmatch(cls.PATTERN, name)):
+            raise ValueError(f"Invalid name: {name}")
+
+        group = match.groupdict()
+        branch = group.get("branch")
+
+        if re.fullmatch(Jet.PATTERN, group["parent"]):
+            parent = Jet.from_name(group["parent"])
+        elif re.fullmatch(FatJet.PATTERN, group["parent"]):
+            parent = FatJet.from_name(group["parent"])
+        elif re.fullmatch(Reclustered.PATTERN, group["parent"]):
+            parent = Reclustered.from_name(group["parent"])
+        else:
+            raise ValueError(f"Invalid parent: {group['parent']}")
+
+        index = retrieve_index(group["index"])
+
+        obj = cls(parent=parent, index=index)
+        obj.branch_name = branch or underscore(cls.__name__)
+
+        return obj
